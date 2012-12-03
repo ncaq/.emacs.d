45250e1002ffd9a78f12237775804832b9569fa2
ncaq <ncaq@ncaq.net>
2012-12-03 21:55:43 +0900

    gitを使わずファイルを削除してしまったので,git add -uで修正

--- profile.d/50_one-key.el
+++
@@ -1,3 +0,0 @@
-;; (require 'one-key-default) ; one-key.el も一緒に読み込んでくれる
-;; (require 'one-key-config) ; one-key.el をより便利にする
-;; (one-key-default-setup-keys) ; one-key- で始まるメニュー使える様になる
--- profile.d/70_twittering-mode.el
+++
@@ -1,6 +0,0 @@
-;;http://emacswiki.org/emacs/TwitteringMode-ja
-(add-to-list 'load-path "~/.emacs.d/bigprogram.d/twittering-mode/")
-
-(require 'twittering-mode)
-
-(setq twittering-use-master-password t)
