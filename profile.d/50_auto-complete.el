33afcbbfae400b917a0e5b4464a6e7b889118e48
ncaq <ncaq@ncaq.net>
2012-12-04 20:32:51 +0900

    auto-complete関係の設定の変更 後helmをダウンロードだけしたけどやっぱり使うのやめた elpaはわからん

--- profile.d/50_auto-complete.el
+++ profile.d/50_auto-complete.el
@@ -5,32 +5,22 @@
 
 (require 'auto-complete)
 (require 'auto-complete-config)
+(global-auto-complete-mode t)
 
 ;;自動で補完画面を出すならt。補完キーを押すまで補完画面を出さないならnil
 (setq ac-auto-start nil)
 
-;;補完画面からさらにそのヘルプ画面が出るまでの遅延（秒）
+;;ヘルプを即表示
 (setq ac-quick-help-delay 0)
 
 ;; 大文字・小文字を区別しない
 (setq ac-ignore-case t)
 
-;;補完キー指定。お好みで。
+;;補完キー指定
 (ac-set-trigger-key "TAB")
+(define-key ac-mode-map (kbd "C-,") 'auto-complete)
+(setq ac-use-menu-map t)
 
-;;http://www.nomtats.com/2010/11/auto-completeelemacs.html
-(defun my-ac-config ()
-  (global-set-key [?\C-,] 'ac-start)
-  ;; C-n/C-p で候補を選択
-  (define-key ac-complete-mode-map "\C-n" 'ac-next)
-  (define-key ac-complete-mode-map "\C-p" 'ac-previous)
-  (setq-default ac-sources '(ac-source-abbrev 
-			     ac-source-dictionary 
-			     ac-source-words-in-same-mode-buffers))
-  (add-hook 'c++-mode-hook 'ac-cc-mode-setup)
-  (add-hook 'emacs-lisp-mode-hook 'ac-emacs-lisp-mode-setup)
-  (add-hook 'c-mode-common-hook 'my-ac-cc-mode-setup)
-  (add-hook 'auto-complete-mode-hook 'ac-common-setup)
-  (global-auto-complete-mode t))
+;;なんか崩れるから最適化しない
+(setq popup-use-optimized-column-computation nil)
 
-(my-ac-config)
--- profile.d/60_auto-complete-clang.el
+++ profile.d/60_auto-complete-clang.el
@@ -2,8 +2,5 @@
 (require 'auto-complete-clang)
 
 (defun my-ac-cc-mode-setup ()
-  (setq-default ac-sources '(ac-source-abbrev ac-source-dictionary ac-source-words-in-same-mode-buffers))
-  (setq ac-sources (append '(ac-source-clang ac-source-yasnippet) ac-sources))
-  (ac-cc-mode-setup)  
-  )
+  (setq ac-sources (append '(ac-source-clang ac-source-yasnippet ac-source-gtags) ac-sources)))
 (add-hook 'c-mode-common-hook 'my-ac-cc-mode-setup)
