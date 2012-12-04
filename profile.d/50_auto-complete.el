5908b0147654a0921f62de0a478bdbd3ab4a6e50
ncaq <ncaq@ncaq.net>
2012-12-04 20:59:31 +0900

    auto-complete.elの辞書が読み込まれていなかったことの修正 load-pathの追加

--- profile.d/50_auto-complete.el
+++ profile.d/50_auto-complete.el
@@ -1,10 +1,11 @@
 (add-to-list 'load-path "~/.emacs.d/bigprogram.d/popup-el/");popup.elが必要
 
 ;;http://cx4a.org/software/auto-complete/manual.ja.html
-(add-to-list 'load-path "~/.emacs.d/bigprogram.d/auto-complete/");ここにインストールしてるのでー
+(add-to-list 'load-path "~/.emacs.d/bigprogram.d/auto-complete/");ここにインストールしてるので
 
-(require 'auto-complete)
 (require 'auto-complete-config)
+(ac-config-default)
+(add-to-list 'ac-dictionary-directories "~/.emacs.d/bigprogram.d/auto-complete/dict/")
 (global-auto-complete-mode t)
 
 ;;自動で補完画面を出すならt。補完キーを押すまで補完画面を出さないならnil
