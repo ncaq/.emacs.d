a6e4eeb694fe17904818fcd3ea849051bd5961e5
ncaq <ncaq@ncaq.net>
2012-12-06 17:26:11 +0900

    よくわからんものを消して,pos-tip.elを使用するように設定

--- profile.d/50_auto-complete.el
+++ profile.d/50_auto-complete.el
@@ -1,8 +1,9 @@
 (add-to-list 'load-path "~/.emacs.d/bigprogram.d/popup-el/");popup.elが必要
+(require 'popup)
+(require 'pos-tip)
 
 ;;http://cx4a.org/software/auto-complete/manual.ja.html
 (add-to-list 'load-path "~/.emacs.d/bigprogram.d/auto-complete/");ここにインストールしてるので
-
 (require 'auto-complete-config)
 (ac-config-default)
 (add-to-list 'ac-dictionary-directories "~/.emacs.d/bigprogram.d/auto-complete/dict/")
@@ -11,8 +12,8 @@
 ;;自動で補完画面を出すならt。補完キーを押すまで補完画面を出さないならnil
 (setq ac-auto-start nil)
 
-;;ヘルプを即表示…しない.崩れるので.
-(setq ac-quick-help-delay 1)
+;;ヘルプを即表示…する,pos-tipを使えば崩れ無くなった
+(setq ac-quick-help-delay 0)
 
 ;; 大文字・小文字を区別しない
 (setq ac-ignore-case t)
@@ -21,7 +22,4 @@
 (ac-set-trigger-key "TAB")
 (define-key ac-mode-map (kbd "C-,") 'auto-complete)
 (setq ac-use-menu-map t)
-
-;;なんか崩れるから最適化しない
-(setq popup-use-optimized-column-computation nil)
 
--- profile.d/50_auto-install.el
+++ profile.d/50_auto-install.el
@@ -1,4 +1,3 @@
 (require 'auto-install)
 ;;http://d.hatena.ne.jp/rubikitch/20091221/autoinstall
 (setq auto-install-directory "~/.emacs.d/install/")
-(auto-install-update-emacswiki-package-name t)
