31fc21f32ffb7f169a6c4de1deae43e45017b741
ncaq <ncaq@ncaq.net>
2013-04-17 22:45:44 +0900

    flymakeを見やすく…なってるのか? http://d.hatena.ne.jp/kitokitoki/20101230
    スマートなコンパイルを実現するらしい奴を導入

--- profile.d/60_flymake.el
+++ profile.d/60_flymake.el
@@ -1,6 +1,7 @@
 ;;flymake
 ;;makeファイルを使わない
 (require 'flymake)
+
 (defun flymake-cc-init ()
   (let* ((temp-file   (flymake-init-create-temp-buffer-copy
 		       'flymake-create-temp-inplace))
@@ -16,7 +17,40 @@
 (setq flymake-run-in-place nil)
 
 ;;警告色が赤だとこのテーマだとめっちゃ見辛い
-(set-face-background 'flymake-errline "#93a1a1")
-(set-face-foreground 'flymake-errline "#202b36")
-(set-face-background 'flymake-warnline "#93a1a1")
-(set-face-foreground 'flymake-warnline "#053040")
+;; (set-face-background 'flymake-errline "#93a1a1")
+;; (set-face-foreground 'flymake-errline "#202b36")
+;; (set-face-background 'flymake-warnline "#93a1a1")
+;; (set-face-foreground 'flymake-warnline "#053000")
+
+;;http://d.hatena.ne.jp/kitokitoki/20101230
+(require 'fringe-helper)
+
+(set-face-background 'flymake-errline nil)    ;既存のフェイスを無効にする
+(set-face-foreground 'flymake-errline nil)
+(set-face-background 'flymake-warnline nil)
+(set-face-foreground 'flymake-warnline nil)
+
+(make-face 'my-flymake-warning-face)
+(set-face-foreground 'my-flymake-warning-face "red")
+(set-face-background 'my-flymake-warning-face "yelow")
+(setq my-flymake-warning-face 'my-flymake-warning-face)
+
+(defvar flymake-fringe-overlays nil)
+(make-variable-buffer-local 'flymake-fringe-overlays)
+
+(defadvice flymake-make-overlay (after add-to-fringe first
+                                       (beg end tooltip-text face mouse-face)
+                                       activate compile)
+  (push (fringe-helper-insert-region
+	 beg end
+	 (fringe-lib-load (if (eq face 'flymake-errline)
+			      fringe-lib-exclamation-mark
+			    fringe-lib-question-mark))
+	 'left-fringe 'my-flymake-warning-face)
+	;; 'left-fringe 'font-lock-warning-face)        
+        flymake-fringe-overlays))
+
+(defadvice flymake-delete-own-overlays (after remove-from-fringe activate
+                                              compile)
+  (mapc 'fringe-helper-remove flymake-fringe-overlays)
+  (setq flymake-fringe-overlays nil))
