cd34714f0fae8be6abee9f5bc833e3f3e9b828c1
ncaq <ncaq@ncaq.net>
2013-04-17 23:23:19 +0900

    これでflymakeの酷い色表示と戦う日々は終わる

--- profile.d/60_flymake.el
+++ profile.d/60_flymake.el
@@ -1,7 +1,37 @@
 ;;flymake
-;;makeファイルを使わない
+;; (require 'flymake)
+
+;;コピペ始め
+;;http://d.hatena.ne.jp/kitokitoki/20101230
+;;エラーをアイコンで表示して,シンタックスハイライトを消してしまうflymakeのエラー表示とはおさらば
+;;処理自体を消す方法は見つからなかったので仕方なく表示だけ消しておいた
+(require 'fringe-helper)
 (require 'flymake)
 
+(defvar flymake-fringe-overlays nil)
+(make-variable-buffer-local 'flymake-fringe-overlays)
+
+(defadvice flymake-make-overlay (after add-to-fringe first
+				       (beg end tooltip-text face mouse-face)
+				       activate compile)
+  (push (fringe-helper-insert-region
+	 beg end
+	 (fringe-lib-load (if (eq face 'flymake-errline)
+			      fringe-lib-exclamation-mark
+			    fringe-lib-question-mark))
+	 'left-fringe 'font-lock-warning-face)
+	flymake-fringe-overlays))
+
+(defadvice flymake-delete-own-overlays (after remove-from-fringe activate
+					      compile)
+  (mapc 'fringe-helper-remove flymake-fringe-overlays)
+  (setq flymake-fringe-overlays nil))
+
+(provide 'flymake-fringe-icons)
+;;コピペ終わり
+
+;;C++でmakeファイルを使わない時の設定
+;;書き捨て
 (defun flymake-cc-init ()
   (let* ((temp-file   (flymake-init-create-temp-buffer-copy
 		       'flymake-create-temp-inplace))
@@ -16,41 +46,43 @@
 (defvar flymake-run-in-place)
 (setq flymake-run-in-place nil)
 
-;;警告色が赤だとこのテーマだとめっちゃ見辛い
-;; (set-face-background 'flymake-errline "#93a1a1")
-;; (set-face-foreground 'flymake-errline "#202b36")
-;; (set-face-background 'flymake-warnline "#93a1a1")
-;; (set-face-foreground 'flymake-warnline "#053000")
 
-;;http://d.hatena.ne.jp/kitokitoki/20101230
-(require 'fringe-helper)
+;;nilにしてしまえ
+(defun flymake-highlight-line (line-no line-err-info-list)
+  "Highlight line LINE-NO in current buffer.
+Perhaps use text from LINE-ERR-INFO-LIST to enhance highlighting."
+  (goto-char (point-min))
+  (forward-line (1- line-no))
+  (let* ((line-beg (point-at-bol))
+	 (line-end (point-at-eol))
+	 (beg      line-beg)
+	 (end      line-end)
+	 (tooltip-text (flymake-ler-text (nth 0 line-err-info-list)))
+	 (face     nil))
 
-(set-face-background 'flymake-errline nil)    ;既存のフェイスを無効にする
-(set-face-foreground 'flymake-errline nil)
-(set-face-background 'flymake-warnline nil)
-(set-face-foreground 'flymake-warnline nil)
+    (goto-char line-beg)
+    (while (looking-at "[ \t]")
+      (forward-char))
 
-(make-face 'my-flymake-warning-face)
-(set-face-foreground 'my-flymake-warning-face "red")
-(set-face-background 'my-flymake-warning-face "yelow")
-(setq my-flymake-warning-face 'my-flymake-warning-face)
+    (setq beg (point))
 
-(defvar flymake-fringe-overlays nil)
-(make-variable-buffer-local 'flymake-fringe-overlays)
+    (goto-char line-end)
+    (while (and (looking-at "[ \t\r\n]") (> (point) 1))
+      (backward-char))
 
-(defadvice flymake-make-overlay (after add-to-fringe first
-                                       (beg end tooltip-text face mouse-face)
-                                       activate compile)
-  (push (fringe-helper-insert-region
-	 beg end
-	 (fringe-lib-load (if (eq face 'flymake-errline)
-			      fringe-lib-exclamation-mark
-			    fringe-lib-question-mark))
-	 'left-fringe 'my-flymake-warning-face)
-	;; 'left-fringe 'font-lock-warning-face)        
-        flymake-fringe-overlays))
+    (setq end (1+ (point)))
 
-(defadvice flymake-delete-own-overlays (after remove-from-fringe activate
-                                              compile)
-  (mapc 'fringe-helper-remove flymake-fringe-overlays)
-  (setq flymake-fringe-overlays nil))
+    (when (<= end beg)
+      (setq beg line-beg)
+      (setq end line-end))
+
+    (when (= end beg)
+      (goto-char end)
+      (forward-line)
+      (setq end (point)))
+
+    ;; (if (> (flymake-get-line-err-count line-err-info-list "e") 0)
+    ;; 	(setq face 'flymake-errline)
+    ;;   (setq face 'flymake-warnline))
+
+    (flymake-make-overlay beg end tooltip-text face nil)))
