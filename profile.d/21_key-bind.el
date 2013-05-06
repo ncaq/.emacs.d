261cd73e0ffd357ab863a448d016161d0e1eb825
ncaq <ncaq@ncaq.net>
2013-05-06 18:51:35 +0900

    頻繁に使ってると押しづらかった

--- profile.d/21_key-bind.el
+++ profile.d/21_key-bind.el
@@ -18,15 +18,16 @@
 (global-set-key			(kbd "C-c e s")	'evernote-search-notes)
 (global-set-key			(kbd "C-c e w")	'evernote-write-note)
 (global-set-key			(kbd "C-c g")	'anything-do-grep)
+(global-set-key			(kbd "C-c h")	'help-command);HHKだとF1押しにくい
 (global-set-key			(kbd "C-c q")	'quoted-insert);C-qの本来の関数
 (global-set-key			(kbd "C-c s")	'sort-lines);ソートする
 (global-set-key			(kbd "C-i")	'code-format-all);全ての文字に対し字下げを行う
-(global-set-key			(kbd "C-j")	'ff-find-other-file);ヘッダファイルに居る場合はソースファイルに,または逆
+(global-set-key			(kbd "C-j")	'recentf-open-most-recent-file);最後に閉じたバッファを開く
 (global-set-key			(kbd "C-m")	'newline-and-indent);改行時にインデント
 (global-set-key			(kbd "C-o")	'anything-find-file)
 (global-set-key			(kbd "C-q")	'kill-buffer-and-window);C-qバッファ閉じる
 (global-set-key			(kbd "C-u")	'kill-whole-line);現在行を削除
-(global-set-key			(kbd "C-z")	'recentf-open-most-recent-file);最後に閉じたバッファを開く
+(global-set-key			(kbd "C-z")	'ff-find-other-file);ヘッダファイルに居る場合はソースファイルに,または逆
 (global-set-key			(kbd "M-,")	'anything-kill-buffers);複数のバッファを簡単に閉じれる
 (global-set-key			(kbd "M-\\")	'delete-horizontal-space);前の改行も消すように
 (global-set-key			(kbd "M-j")	'open-junk-file);残るscratch
@@ -35,7 +36,6 @@
 (global-set-key			(kbd "M-n")	'scroll-up-1);http://d.hatena.ne.jp/uhiaha888/20101110/1289399913
 (global-set-key			(kbd "M-p")	'scroll-down-1);カーソルを移動せずに画面を一行ずつスクロール
 (global-set-key			(kbd "M-q")	'delete-other-windows);他のウインドウを閉じる
-(global-set-key			(kbd "C-c h")	'help-command);HHKだとF1押しにくい
 
 ;;C-hをBackSpaceに変更
 (global-unset-key (kbd "C-h"))
