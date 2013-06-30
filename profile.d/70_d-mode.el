d95c5ae3a292c188780957dbe767dbebdeb17228
ncaq <ncaq@ncaq.net>
2013-06-30 18:39:20 +0900

    .recentfのバックアップを作らないように…出来てるのかな…?
    d-modoでauto-complete-clangが働かないようにしたけどやっつけな方法だから上手く行ってるかわからない

--- profile.d/70_d-mode.el
+++ profile.d/70_d-mode.el
@@ -1,7 +1,22 @@
 (autoload 'd-mode "d-mode" "Major mode for editing D code." t);D言語
 
-(defun ncaq-d-mode-set ()
+;;わからん
+;;(require 'cl)
+;; (defun clang-delete ()
+;;   (setq ac-sources
+;; 	 (remove-if
+;; 	  (lambda (x)
+;; 	    (equal x ac-source-clang))
+;; 	 ac-sources)))
+
+;;d-modeはcc-modeを改造したものなため,d-modeを起動するとD言語書いてるのにC++の補完システムが起動してしまう.
+;;やっつけでこれを防ぐ
+(defun clang-delete()
+  (pop ac-sources))
+
+(defun ncaq-d-mode-setup ()
   (flymake-d-load)
   (subword-mode t)
+  (clang-delete)
   (local-set-key (kbd "C-i") 'code-format-c))
-(add-hook 'd-mode-hook 'ncaq-d-mode-set)
+(add-hook 'd-mode-hook 'ncaq-d-mode-setup)
