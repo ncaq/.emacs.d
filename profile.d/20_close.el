2afc069fd044129f89f73d76abc07ea3e9a150fa
ncaq <ncaq@ncaq.net>
2013-07-03 20:40:36 +0900

    公式でスクリプトに実行権限付加機能あったっぽい

--- profile.d/20_close.el
+++ profile.d/20_close.el
@@ -1,15 +1,6 @@
-;;閉じる,保存するときに色々するもの
-(defun make-file-executable ()
-  "Make the file of this buffer executable, when it is a script source."
-  (save-restriction
-    (widen)
-    (if (string= "#!" (buffer-substring-no-properties 1 (min 3 (point-max))))
-	(let ((name (buffer-file-name)))
-	  (or (equal ?. (string-to-char (file-name-nondirectory name)))
-	      (let ((mode (file-modes name)))
-		(set-file-modes name (logior mode (logand (/ mode 4) 73)))
-		(message (concat "Wrote " name " (+x)"))))))))
-(add-hook 'after-save-hook 'make-file-executable)
+;;スクリプトに実行権限付加
+(add-hook 'after-save-hook
+          'executable-make-buffer-file-executable-if-script-p)
 
 ;;; 最終行に必ず一行挿入する
 (setq require-final-newline t)
