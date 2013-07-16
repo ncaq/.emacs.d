d5d5cc70e14030c05de62ee3b8320d829d9e0746
ncaq <ncaq@ncaq.net>
2013-07-16 17:26:24 +0900

    melpa追加

--- profile.d/05_package.el
+++ profile.d/05_package.el
@@ -1,3 +1,4 @@
 (require 'package)
 (add-to-list 'package-archives '("marmalade" . "http://marmalade-repo.org/packages/"));リポジトリにMarmaladeを追加
+(add-to-list 'package-archives '("melpa" . "http://melpa.milkbox.net/packages/"));;melpaも追加
 (package-initialize);インストールしたパッケージにロードパスを通してロードする
