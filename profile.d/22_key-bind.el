ec7e027110d939219b5652e056ea464ca0c43fad
ncaq <ncaq@ncaq.net>
2013-08-29 23:12:29 +0900

    またKeybind変更

--- profile.d/22_key-bind.el
+++ profile.d/22_key-bind.el
@@ -8,7 +8,6 @@
 (global-set-key			(kbd "C-;")	'align-regexp);揃える(正規表現)
 (global-set-key			(kbd "C-M-S-q")	'close-all-buffers);バッファを全て閉じる.まともに動かなくなるのですぐに終了すること
 (global-set-key			(kbd "C-M-d")	'kill-paragraph);段落削除
-(global-set-key			(kbd "C-M-i")	'text-adjust-selective);全角記号とかそういうゴミな文字を変換する
 (global-set-key			(kbd "C-M-l")	'sort-lines);ソートする
 (global-set-key			(kbd "C-M-n")	'scroll-up-1);http://d.hatena.ne.jp/uhiaha888/20101110/1289399913
 (global-set-key			(kbd "C-M-p")	'scroll-down-1);カーソルを移動せずに画面を一行ずつスクロール
@@ -16,8 +15,11 @@
 (global-set-key			(kbd "C-M-z")	'recentf-open-most-recent-file);最後に閉じたバッファを開く
 (global-set-key			(kbd "C-S-d")	'delete-horizontal-space);スペースを一気に消す
 (global-set-key			(kbd "C-a")	'move-beginning-of-line-Visual-Stdio-like);;Visual StdioライクなC-a
+(global-set-key			(kbd "C-c a")	'text-adjust-selective);全角記号とかそういうゴミな文字を変換する
 (global-set-key			(kbd "C-c c")	'mode-compile);かしこいコンパイルコマンド実行
+(global-set-key			(kbd "C-c f")	'code-format-all);全ての文字に対し字下げを行う
 (global-set-key			(kbd "C-c h")	'help-command);HHKだとF1押しにくい
+(global-set-key			(kbd "C-c j")	'open-junk-file);残るscratch
 (global-set-key			(kbd "C-j")	'anything-do-grep);インクリメント串刺し検索
 (global-set-key			(kbd "C-m")	'newline-and-indent);改行時にインデント
 (global-set-key			(kbd "C-o")	'overwrite-mode);所謂insertモード
@@ -27,8 +29,6 @@
 (global-set-key			(kbd "C-z")	'quoted-insert);C-qの本来の関数
 (global-set-key			(kbd "M-,")	'ibuffer);もう一つのバッファーリスト
 (global-set-key			(kbd "M-\\")	'delete-horizontal-space);前の改行も消すように
-(global-set-key			(kbd "M-i")	'code-format-all);全ての文字に対し字下げを行う
-(global-set-key			(kbd "M-j")	'open-junk-file);残るscratch
 (global-set-key			(kbd "M-l")	'sdic)
 (global-set-key			(kbd "M-m")	'through-newline);
 (global-set-key			(kbd "M-n")	'forward-paragraph)
