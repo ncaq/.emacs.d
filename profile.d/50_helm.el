764b4390c8533acbe6ecf29d63c11c2a9cdafdd9
ncaq <ncaq@ncaq.net>
2013-09-05 21:24:22 +0900

    もっと簡単な方法があった

--- profile.d/50_helm.el
+++ profile.d/50_helm.el
@@ -6,11 +6,5 @@
 
 (require 'helm-ag)
 
-;;helmのBufferを縦に表示する ワイドディスプレイの時代だし
-(defun helm-default-display-buffer (buf)
-  (if helm-full-frame
-      (switch-to-buffer buf)
-    (progn
-      (delete-other-windows)
-      (split-window (selected-window) nil t)
-      (pop-to-buffer buf))))
+;;縦(右)に表示する
+(setq helm-split-window-default-side 'right)
