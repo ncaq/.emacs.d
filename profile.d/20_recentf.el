147ba32c36fd08301e3950f1a804ce6c68aa07e6
ncaq <ncaq@ncaq.net>
2013-10-01 23:24:50 +0900

    fix fix fix

--- profile.d/20_recentf.el
+++ profile.d/20_recentf.el
@@ -1,11 +1,8 @@
 (require 'recentf)
+(require 'recentf-purge-tramp)
 (setq recentf-max-menu-items  100000);;メニュー表示件数
 (setq recentf-max-saved-items 100000);;保存件数
 
-(defun recentf-clean ()
-  (lambda ()
-    (run-at-time t 3600 'recentf-cleanup);定期的に削除をチェック
-    (run-with-idle-timer 300 t 'recentf-cleanup)));一定時間放置したら削除をチェック
-(add-hook 'after-init-hook recentf-clean)
-
-(require 'recentf-purge-tramp)
+(add-hook 'after-init-hook
+	  '(lambda ()
+	     (run-with-idle-timer 300 t 'recentf-cleanup)));一定時間放置したら削除をチェック
