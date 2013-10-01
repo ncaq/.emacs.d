0ab9e3f784164c41b088ba2d8ecb6d40e9b606c0
ncaq <ncaq@ncaq.net>
2013-10-01 22:58:24 +0900

    recentf realy clean bug fix.

--- profile.d/20_recentf.el
+++ profile.d/20_recentf.el
@@ -2,7 +2,10 @@
 (setq recentf-max-menu-items  100000);;メニュー表示件数
 (setq recentf-max-saved-items 100000);;保存件数
 
-(run-at-time t 3600 'recentf-cleanup);定期的に削除をチェック
-(run-with-idle-timer 300 t 'auto-save-buffers);一定時間放置したら削除をチェック
+(defun recentf-clean ()
+  (lambda ()
+    (run-at-time t 3600 'recentf-cleanup);定期的に削除をチェック
+    (run-with-idle-timer 300 t 'recentf-cleanup)));一定時間放置したら削除をチェック
+(add-hook 'after-init-hook recentf-clean)
 
 (require 'recentf-purge-tramp)
