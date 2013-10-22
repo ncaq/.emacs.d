950307ac785feeb1df5fd6d4fbdb5c8744d09855
ncaq <ncaq@ncaq.net>
2013-10-22 18:35:27 +0900

    ls option

--- profile.d/20_directory.el
+++ profile.d/20_directory.el
@@ -1,4 +1,4 @@
-(setq dired-listing-switches "-hFAvilaD");diredでソート順を設定
+(setq dired-listing-switches "-hFAvlD");diredでソート順を設定
 
 (autoload 'open-junk-file "open-junk-file");;使い捨てないscratch
 (defvar open-junk-file-directory)
