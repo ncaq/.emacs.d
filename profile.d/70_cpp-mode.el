5e4f12baedf547f19a7dfdab7021e356e765f3e3
ncaq <ncaq@ncaq.net>
2013-11-14 22:28:25 +0900

    defvarとsetqの違いがよくわかっていないというゴミ

--- profile.d/70_cpp-mode.el
+++ profile.d/70_cpp-mode.el
@@ -2,15 +2,15 @@
 
 (add-to-list 'auto-mode-alist '("\\.h$" . c++-mode));*.hをc++モードで開く
 
-(require 'flycheck-autoloads)
+(require 'flycheck)
 
 (defun ncaq-c++-mode-set ()
   (c-set-style "bsd");http://www.02.246.ne.jp/~torutk/cxx/emacs/indentation.html
   (local-set-key (kbd "M-z") 'code-format-c))
 (add-hook 'c-mode-common-hook 'ncaq-c++-mode-set)
 
-;; (add-hook 'c-mode-hook (lambda ()
-;; 			 (defvar flycheck-clang-language-standard "c11")))
+(add-hook 'c-mode-hook (lambda ()
+			 (setq flycheck-clang-language-standard "c11")))
 
 (add-hook 'c++-mode-hook (lambda ()
-			   (defvar flycheck-clang-language-standard "c++11")))
+			   (setq flycheck-clang-language-standard "c++11")))
