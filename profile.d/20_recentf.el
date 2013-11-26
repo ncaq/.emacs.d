fe95f128d743e52e7f5deb389b7fc2bad81d193a
ncaq <ncaq@ncaq.net>
2013-11-26 13:20:33 +0900

    recentfのDefaultの保存件数が思ったより少なかった git-commitのfillが鬱陶しいので削除

--- profile.d/20_recentf.el
+++ profile.d/20_recentf.el
@@ -1,6 +1,9 @@
-(custom-set-variables '(recentf-save-file "~/Dropbox/sync/.recentf"))
-
 (require 'recentf)
+
+(custom-set-variables '(recentf-save-file "~/Dropbox/sync/.recentf"))
+(setq recentf-max-menu-items 20)
+(setq recentf-max-saved-items 500)
+
 (recentf-mode 1)
 (require 'recentf-ext)
 (require 'recentf-purge-tramp)
--- profile.d/60_git.el
+++ profile.d/60_git.el
@@ -1,5 +1,7 @@
 (require 'magit)
 (define-key magit-mode-map (kbd "S") (lambda () (interactive)(magit-stage-all 4)))
 
+(remove-hook 'git-commit-mode-hook 'turn-on-auto-fill)
+
 (require 'git-gutter-fringe)
 (global-git-gutter-mode t)
