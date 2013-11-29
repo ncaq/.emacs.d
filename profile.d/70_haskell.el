f8675f4ab894030d0e161b5dec14a1680aab4fda
ncaq <ncaq@ncaq.net>
2013-11-29 14:17:18 +0900

    別に追加する必要なかった…

--- profile.d/70_haskell.el
+++ profile.d/70_haskell.el
@@ -1,17 +1,10 @@
 ;;Haskell関係
-(require 'haskell-mode-autoloads)
-(add-hook 'haskell-mode-hook 'turn-on-haskell-doc-mode)
-(add-hook 'haskell-mode-hook 'turn-on-haskell-indentation)
+(require 'haskell-mode)
+(add-hook 'haskell-mode-hook 'haskell-doc-mode)
+(add-hook 'haskell-mode-hook 'haskell-indentation-mode)
 
 (require 'ghc)
 (add-hook 'haskell-mode-hook 'ghc-init)
 
 (require 'auto-complete)
 (add-to-list 'ac-modes 'haskell-mode)
-
-
-(defun ncaq-haskell-conf ()
-  (add-to-list 'ac-sources 'ac-source-ghc-mod);ghc-modのソースをautocompleteに追加
-  )
-
-(add-hook 'haskell-mode-hook 'ncaq-haskell-conf)
