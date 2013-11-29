08c22e55875dd1ddafcc69519ec0760ecccad6dc
ncaq <ncaq@ncaq.net>
2013-11-29 14:49:03 +0900

    ghciの設定追加

--- profile.d/70_haskell.el
+++ profile.d/70_haskell.el
@@ -2,6 +2,7 @@
 (require 'haskell-mode)
 (add-hook 'haskell-mode-hook 'haskell-doc-mode)
 (add-hook 'haskell-mode-hook 'haskell-indentation-mode)
+(add-hook 'inferior-haskell-mode-hook 'ghci-completion-mode)
 
 (require 'ghc)
 (add-hook 'haskell-mode-hook 'ghc-init)
