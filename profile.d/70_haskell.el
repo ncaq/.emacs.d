2dd341bc875dd5a6c0a8cceb9e6a94272023231c
ncaq <ncaq@ncaq.net>
2013-12-03 21:06:34 +0900

    define-keyのエラーがいろいろな悪影響を及ぼしていた あと package update

--- profile.d/70_haskell.el
+++ profile.d/70_haskell.el
@@ -2,14 +2,16 @@
 (require 'haskell-indentation)
 (require 'haskell-doc)
 (require 'ghc)
-;; (add-hook 'haskell-mode-hook 'haskell-indentation-mode)
-;; (add-hook 'haskell-mode-hook 'haskell-doc-mode)
+
+(add-hook 'haskell-mode-hook 'haskell-indentation-mode)
+(add-hook 'haskell-mode-hook 'haskell-doc-mode)
+(add-hook 'haskell-mode-hook 'haskell-decl-scan-mode)
 
 (add-hook 'inferior-haskell-mode-hook 'ghci-completion-mode)
 
 ;;勝手にflymakeを割り当てられる
-(setq ghc-previous-key    'nil)
-(setq ghc-next-key        'nil)
+(setq ghc-previous-key    (kbd "C-c C-c p"))
+(setq ghc-next-key        (kbd "C-c C-c n"))
 
 (require 'auto-complete)
 (add-to-list 'ac-modes 'haskell-mode)
@@ -17,6 +19,4 @@
 (defun ncaq-haskell ()
   (ghc-init)
   (add-to-list 'ac-sources 'ac-source-ghc-mod))
-  ;; (turn-on-haskell-indentation)
-  ;; (turn-on-haskell-doc))
 (add-hook 'haskell-mode-hook 'ncaq-haskell)
