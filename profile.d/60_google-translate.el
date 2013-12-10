16f8cf27ca6c265dcdb722be29f0ae3bfa8ad835
ncaq <ncaq@ncaq.net>
2013-12-11 05:11:09 +0900

    key-bind変更

--- profile.d/60_google-translate.el
+++ profile.d/60_google-translate.el
@@ -5,4 +5,4 @@
   '(google-translate-default-target-language "ja"))
 
 (global-set-key (kbd "C-c t") 'google-translate-at-point)
-(global-set-key (kbd "C-c r") 'google-translate-at-point-reverse)
+(global-set-key (kbd "C-c n") 'google-translate-at-point-reverse)
