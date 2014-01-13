e8b0436e8fa7a3576e641275f14d25f46f536f18
ncaq <ncaq@ncaq.net>
2014-01-13 11:04:15 +0900

    移動

--- profile.d/70_cpp.el
+++ profile.d/70_cpp.el
@@ -3,11 +3,6 @@
 
 (add-to-list 'auto-mode-alist '("\\.h$" . c++-mode));*.hをc++モードで開く
 
-(defun ncaq-c++-mode-set ()
-  (c-set-style "bsd")
-  (define-key c-mode-base-map [remap indent-whole-buffer] 'indent-brackets-whole-buffer))
-(add-hook 'c-mode-common-hook 'ncaq-c++-mode-set)
-
 (add-hook 'c-mode-hook (lambda ()
 			 (setq flycheck-clang-language-standard "c11")))
 
@@ -16,3 +11,8 @@
 
 (require 'c-eldoc)
 (add-hook 'c-mode-common-hook 'c-turn-on-eldoc-mode)
+
+(defun ncaq-c++-mode-set ()
+  (c-set-style "bsd")
+  (define-key c-mode-base-map [remap indent-whole-buffer] 'indent-brackets-whole-buffer))
+(add-hook 'c-mode-common-hook 'ncaq-c++-mode-set)
--- profile.d/70_d.el
+++ profile.d/70_d.el
@@ -1,8 +1,8 @@
 (require 'd-mode)
 (require 'auto-complete)
 
+(add-to-list 'ac-modes 'd-mode)
+
 (defun ncaq-d-mode-setup ()
   (define-key d-mode-map [remap indent-whole-buffer] 'indent-brackets-whole-buffer))
 (add-hook 'd-mode-hook 'ncaq-d-mode-setup)
-
-(add-to-list 'ac-modes 'd-mode)
