104c6e27bb273152d0078ad960b815b4cb70134d
ncaq <ncaq@ncaq.net>
2014-01-28 18:49:43 +0900

    評価間違えた

--- profile.d/70_haskell.el
+++ profile.d/70_haskell.el
@@ -18,7 +18,7 @@
   (add-hook 'after-save-hook 'ghc-import-module))
 (add-hook 'haskell-mode-hook 'ncaq-haskell)
 
-(define-key 'haskell-mode-map (kbd "C-M-m") nil)
+(define-key haskell-mode-map (kbd "C-M-m") nil)
 
 ;;勝手にflymakeを割り当てられる
 (setq ghc-previous-key    (kbd "C-c C-c p"))
