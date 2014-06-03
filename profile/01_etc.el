3a1df22addf5def16c78918b5662642f47d4175b
ncaq <ncaq@ncaq.net>
2014-06-03 13:55:29 +0900

    線形なので数を減らす

--- profile/01_etc.el
+++ profile/01_etc.el
@@ -1,20 +1,20 @@
 (custom-set-variables
- '(mode-require-final-newline nil)
  '(backup-directory-alist '(("" . "~/.emacs.d/emacs-backup")))
  '(blink-matching-paren nil);括弧移動無効
  '(delete-by-moving-to-trash t);ごみ箱を有効
- '(trash-directory "~/trash/")
- '(history-length 5000);ミニバッファの履歴の保存数を増やす
+ '(history-length 500);ミニバッファの履歴の保存数を増やす
+ '(indent-tabs-mode nil) 
  '(kept-new-versions 100000);新しいものをいくつ残すか
  '(kept-old-versions 100000);古いものをいくつ残すか
  '(make-backup-files t);バックアップファイルを作成する。
- '(message-log-max 5000);ログの記録行数を増やす.
+ '(message-log-max 1000);ログの記録行数を増やす.
+ '(mode-require-final-newline nil)
  '(read-buffer-completion-ignore-case t);大文字と小文字を区別しない バッファ名
  '(read-file-name-completion-ignore-case t);ファイル名
- '(version-control t);複数のバックアップを残します。世代。
- '(x-select-enable-clipboard t);クリップボードをX11と共有
- '(indent-tabs-mode nil) 
- '(save-place t))
+ '(save-place t)
+ '(trash-directory "~/trash/")
+ '(version-control t);複数バックアップ
+ '(x-select-enable-clipboard t));クリップボードをX11と共有
 
 (add-hook 'after-save-hook 'executable-make-buffer-file-executable-if-script-p);スクリプトに実行権限付加
 (fset 'yes-or-no-p 'y-or-n-p);"yes or no"を"y or n"に
