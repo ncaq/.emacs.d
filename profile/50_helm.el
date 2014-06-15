cf0c4a7588a327dfcda0db34adc2edcf909c1422
ncaq <ncaq@ncaq.net>
2014-06-15 14:49:27 +0900

    helm conflict tramp

--- profile/50_helm.el
+++ profile/50_helm.el
@@ -3,7 +3,7 @@
 (require 'helm-mode)
 (helm-mode)
 
-(add-to-list 'helm-completing-read-handlers-alist '(find-file . nil));helmを無効にする関数リスト
+(add-to-list 'helm-completing-read-handlers-alist '(find-file find-file-at-point));helmを無効にする関数リスト
 (custom-set-variables '(helm-samewindow t);今のウインドウに表示
                       '(helm-buffer-max-length 50));デフォルトはファイル名を短縮する区切りが20
 
