5761a333dd567293e0b7c0595a35a8af1425b636
ncaq <ncaq@ncaq.net>
2014-07-09 19:55:17 +0900

    fix parameter

--- profile/30_processing.el
+++ profile/30_processing.el
@@ -5,12 +5,8 @@
 
 (require 'auto-complete)
 (defun processing-mode-init ()
-  (make-local-variable 'ac-sources)
-  (setq ac-sources '(ac-source-dictionary ac-source-yasnippet))
   (make-local-variable 'ac-user-dictionary)
   (setq ac-user-dictionary (append processing-functions
                                    processing-builtins
                                    processing-constants)))
-
-(add-to-list 'ac-modes 'processing-mode)
 (add-hook 'processing-mode-hook 'processing-mode-init)
