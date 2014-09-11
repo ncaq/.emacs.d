6e013135d92e5c92db488cd729115805e96e8962
ncaq <ncaq@ncaq.net>
2014-09-12 00:53:43 +0900

    format recentf

--- profile/10_history.el
+++ profile/10_history.el
@@ -7,36 +7,36 @@
  '(backup-directory-alist '(("" . "~/.backup")))
  '(tramp-auto-save-directory "~/.backup")
 
- '(history-length 200)                  ;ミニバッファの履歴の保存数を増やす
- '(kept-new-versions 100)               ;backupに新しいものをいくつ残すか
- '(kept-old-versions 100)               ;backupに古いものをいくつ残すか
+ '(history-length 100)                  ;ミニバッファの履歴の保存数を増やす
+ '(kept-new-versions 500)               ;backupに新しいものをいくつ残すか
+ '(kept-old-versions 500)               ;backupに古いものをいくつ残すか
  '(message-log-max 1000)                ;ログの記録行数を増やす.
- 
- '(savehist-mode 1)                     ;minibufferの履歴を保存する
- '(savehist-ignored-variables '(file-name-history))
 
- '(desktop-save-mode 1)
+ '(recentf-max-saved-items 200)
+ '(recentf-auto-cleanup 1000)
+ '(recentf-exclude '(
+                     "TAGS"
+                     "\\.backup"
+                     "\\.elc$"
+                     "\\.o$"
+                     "\\.undohist"
+                     "trash"
+                     "~$"
+                     ))
+ 
+ '(savehist-ignored-variables '(file-name-history))
  )
+
+(desktop-save-mode 1)
+(savehist-mode 1)
 
 (defun recentf-setup ()
   "recentfによる .recentf の読み込みで,バックアップ位置がバグることへの対策"
-  (custom-set-variables
-   '(recentf-mode 1)
-   '(recentf-max-saved-items 200)
-   '(recentf-auto-cleanup 1000)
-   '(recentf-exclude '(
-                       "TAGS"
-                       "\\.backup"
-                       "\\.elc$"
-                       "\\.o$"
-                       "\\.undohist"
-                       "trash"
-                       "~$"
-                       )))
   (require 'recentf)
   (require 'recentf-ext)
   (require 'recentf-purge-tramp)
-
+  
+  (recentf-mode 1)
   (defvar recentf-list)
   (setq file-name-history recentf-list)
   )
