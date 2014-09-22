d09f7e7a1c2a10464011526c78cf4f453f560d2a
ncaq <ncaq@ncaq.net>
2014-09-22 22:21:17 +0900

    fix warning kill buffer when modified undohist-always-recover was obsolete.Because ssh -> scp

--- profile/50_undo.el
+++ profile/50_undo.el
@@ -1,6 +1,6 @@
 ;; -*- lexical-binding: t -*-
 
-(require 'undo-tree);undoをtreeに,C-x C-uで起動
+(require 'undo-tree)
 
 (defun strict-global-undo-tree-mode ()
   (undo-tree-mode 1))
@@ -13,19 +13,11 @@
 (define-key undo-tree-visualizer-mode-map (kbd "t")   'undo-tree-visualize-undo)
 
 (require 'undohist);undoをファイル閉じても保存
-(custom-set-variables '(undohist-directory "~/.undohist"))
+(custom-set-variables
+ '(undohist-directory "~/.undohist")
+ '(undohist-ignored-files '("COMMIT_EDITMSG"))
+ )
 (undohist-initialize)
-
-;;履歴を常にtrampファイルでも復元する
-(defadvice yes-or-no-p (around always-yes)
-  (setq ad-return-value t))
-
-(defadvice undohist-recover-1 (around undohist-always-recover)
-  (ad-activate-regexp "always-yes")
-  ad-do-it
-  (ad-deactivate-regexp "always-yes"))
-
-(ad-activate-regexp "always-yes")
 
 (require 'point-undo)
 (global-set-key (kbd "M-/") 'point-undo)
