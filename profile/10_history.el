348f28ae69093ff280e905eae0c87e4424426934
ncaq <ncaq@ncaq.net>
2014-11-06 10:04:55 +0900

    changed: file backup path

--- profile/10_history.el
+++ profile/10_history.el
@@ -1,15 +1,15 @@
 ;; -*- lexical-binding: t -*-
 
 (custom-set-variables
- '(make-backup-files t)                 ;バックアップファイルを作成する。
- '(version-control t)                   ;複数バックアップ
- 
- '(backup-directory-alist '(("" . "~/.backup")))
- '(tramp-auto-save-directory "~/.backup")
+ '(make-backup-files t)              ;バックアップファイルを作成する。
+ '(version-control t)                ;複数バックアップ
+ '(delete-old-versions t)
+ '(kept-new-versions 50)             ;backupに新しいものをいくつ残すか
+ '(kept-old-versions 50)             ;backupに古いものをいくつ残すか
+ '(backup-directory-alist '(("" . "~/.file-backup")))
+ '(tramp-auto-save-directory "~/.file-backup")
 
  '(history-length 500)                  ;ミニバッファの履歴の保存数を増やす
- '(kept-new-versions 500)               ;backupに新しいものをいくつ残すか
- '(kept-old-versions 500)               ;backupに古いものをいくつ残すか
  '(message-log-max 5000)                ;ログの記録行数を増やす.
 
  '(save-place t)                        ;fileのカーソル位置保存
@@ -17,9 +17,10 @@
  '(recentf-max-saved-items 500)
  '(recentf-auto-cleanup 1000)
  '(recentf-exclude '(
+                     "COMMIT_EDITMSG$"
                      "TAGS"
-                     "\\.backup"
                      "\\.elc$"
+                     "\\.file-backup"
                      "\\.o$"
                      "\\.undohist"
                      "trash"
