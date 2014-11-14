7568dad13db3cbc7df218d500baf5c054f31c479
ncaq <ncaq@ncaq.net>
2014-11-14 23:31:34 +0900

    fixed: helm-gtags-mode setting

--- profile/50_tags.el
+++ profile/50_tags.el
@@ -1,22 +1,21 @@
 ;; -*- lexical-binding: t -*-
 
-(eval-after-load 'gtags
-  '(progn
-     (custom-set-variables
-      '(helm-gtags-auto-update t)
-      '(helm-gtags-ignore-case t)
-      )
+(with-eval-after-load 'helm-gtags
+  (custom-set-variables
+   '(helm-gtags-auto-update t)
+   '(helm-gtags-ignore-case t)
+   )
 
-     (define-key helm-gtags-mode-map (kbd "C-.")   'helm-gtags-find-tag-from-here)
-     (define-key helm-gtags-mode-map (kbd "C->")   'helm-gtags-find-rtag)
-     (define-key helm-gtags-mode-map (kbd "C-M-.") 'helm-gtags-select)
-     (define-key helm-gtags-mode-map (kbd "C-c .") 'gtags-visit-rootdir)
-     (define-key helm-gtags-mode-map (kbd "M-.")   'helm-gtags-pop-stack)
+  (define-key helm-gtags-mode-map (kbd "C-.")   'helm-gtags-find-tag-from-here)
+  (define-key helm-gtags-mode-map (kbd "C->")   'helm-gtags-find-rtag)
+  (define-key helm-gtags-mode-map (kbd "C-M-.") 'helm-gtags-select)
+  (define-key helm-gtags-mode-map (kbd "C-c .") 'gtags-visit-rootdir)
+  (define-key helm-gtags-mode-map (kbd "M-.")   'helm-gtags-pop-stack)
 
-     (defun ncaq/add-to-ac-sources-gtags ()
-       (add-to-list 'ac-sources 'ac-source-gtags))
-     (add-hook 'helm-gtags-mode-hook 'ncaq/add-to-ac-sources-gtags)
-     ))
+  (defun ncaq/add-to-ac-sources-gtags ()
+    (add-to-list 'ac-sources 'ac-source-gtags))
+  (add-hook 'helm-gtags-mode-hook 'ncaq/add-to-ac-sources-gtags)
+  )
 
 (add-hook 'c-mode-hook    'helm-gtags-mode)
 (add-hook 'c++-mode-hook  'helm-gtags-mode)
