8d62456426b130700c4c3e326ddb59c93e763822
ncaq <ncaq@ncaq.net>
2015-01-01 19:43:25 +0900

    helm setting: remove command of helm-buffers-list-selective and helm-for-files alternate is custom-set-variable helm-boring-buffer-regexp-list

--- profile/50_helm.el
+++ profile/50_helm.el
@@ -1,25 +1,18 @@
 ;; -*- lexical-binding: t -*-
 
 (require 'helm-mode)
+(require 'helm-config)
+
 (helm-mode 1)
 (helm-descbinds-mode 1)
 
 (custom-set-variables
  '(helm-boring-buffer-regexp-list '("\\*"))
- '(helm-buffer-max-length 50) ;デフォルトはファイル名を短縮する区切りが20
+ '(helm-boring-buffer-regexp-list (cons "\\*tramp" (cons "\\*magit" helm-boring-buffer-regexp-list)))
  '(helm-buffer-max-len-mode 25)         ;モードを短縮する基準
+ '(helm-buffer-max-length 50)           ;デフォルトはファイル名を短縮する区切りが20
  '(helm-samewindow t)                   ;ウインドウ全体に表示
  )
-
-(defun helm-buffers-list-selective ()
-  (interactive)
-  (setq helm-boring-buffer-regexp-list '("\\*"))
-  (helm-buffers-list))
-
-(defun helm-for-files-list-other-helm ()
-  (interactive)
-  (setq helm-boring-buffer-regexp-list '("\\` " "\\*helm" "\\*Echo Area" "\\*Minibuf" "\\*tramp"))
-  (helm-for-files))
 
 (define-key helm-buffer-map        (kbd "C-s") 'nil)
 (define-key helm-generic-files-map (kbd "C-s") 'nil)
