6ef35611552f8f5f0b0adff18868a7a87213a101
ncaq <ncaq@ncaq.net>
2015-01-20 10:26:43 +0900

    Drop scss warning window setting. It is conflict ag.el.

--- profile/80_web.el
+++ profile/80_web.el
@@ -6,10 +6,12 @@
     '(
       ("\\.html\\'"   . web-mode)
       ("\\.xhtml\\'"  . web-mode)
+      ("\\.php\\'"  . web-mode)
       )
     auto-mode-alist)))
 
-(eval-after-load 'web-mode
-  '(progn
-    (define-key web-mode-map (kbd "M-c") 'web-mode-element-close)
-    ))
+(with-eval-after-load 'web-mode
+  (define-key web-mode-map (kbd "M-c") 'web-mode-element-close)
+  )
+
+(custom-set-variables '(scss-sass-options '("--cache-location" "'/tmp/.sass-cache/'" "--style" "expanded")))
