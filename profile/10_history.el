1a771a72623188d934a2a0ea3a66541b813c1884
ncaq <ncaq@ncaq.net>
2015-02-04 20:07:21 +0900

    10_history: fix saveplace.el enable setting add auto-save-file-name-transforms setting to move "/tmp/"

--- profile/10_history.el
+++ profile/10_history.el
@@ -1,18 +1,19 @@
 ;; -*- lexical-binding: t -*-
 
+(defvar backup-directory "~/.file-backup/")
+
 (custom-set-variables
  '(make-backup-files t)              ;バックアップファイルを作成する。
  '(version-control t)                ;複数バックアップ
  '(delete-old-versions t)
  '(kept-new-versions 50)             ;backupに新しいものをいくつ残すか
  '(kept-old-versions 50)             ;backupに古いものをいくつ残すか
- '(backup-directory-alist '(("" . "~/.file-backup")))
- '(tramp-auto-save-directory "~/.file-backup")
+ '(backup-directory-alist `(("" . ,backup-directory)))
+ '(tramp-auto-save-directory backup-directory)
 
- '(history-length 500)                  ;ミニバッファの履歴の保存数を増やす
+ '(auto-save-file-name-transforms `((".*" ,temporary-file-directory t)))
+
  '(message-log-max 5000)                ;ログの記録行数を増やす.
-
- '(save-place t)                        ;fileのカーソル位置保存
 
  '(recentf-max-saved-items 500)
  '(recentf-auto-cleanup 1000)
@@ -20,19 +21,22 @@
                      "COMMIT_EDITMSG$"
                      "TAGS"
                      "\\.elc$"
-                     "\\.file-backup"
                      "\\.o$"
-                     "\\.undo-tree"
-                     "trash"
                      "~$"
+                     
+                     "\\.file-backup/"
+                     "\\.undo-tree/"
+                     "trash/"
                      ))
 
  '(desktop-restore-frames nil)
  '(savehist-ignored-variables '(file-name-history))
+ '(history-length 500)             ;ミニバッファの履歴の保存数を増やす
  )
 
 (desktop-save-mode 1)
 (savehist-mode 1)
+(toggle-save-place-globally t)          ;fileのカーソル位置保存
 
 (defvar recentf-list)
 (defun sync-file-name-history-from-recentf ()
@@ -43,7 +47,7 @@
   (require 'recentf)
   (require 'recentf-ext)
   (require 'recentf-purge-tramp)
-  
+
   (recentf-mode 1)
   (run-with-idle-timer 600 t 'sync-file-name-history-from-recentf)
   )
