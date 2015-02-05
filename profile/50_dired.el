64372bbc2010835a40ad6784d47c8d079e042503
ncaq <ncaq@ncaq.net>
2015-02-06 00:25:08 +0900

    50_dired.el: use ls-first-ln-dir remove atool settings

--- profile/50_dired.el
+++ profile/50_dired.el
@@ -2,24 +2,10 @@
 
 (require 'dired)
 
-(custom-set-variables '(dired-listing-switches "-AFhvLl --group-directories-first")) ;diredが使うlsオプションの設定
-
-;;atoolの設定
-;;http://d.hatena.ne.jp/mooz/20110911/p1
-(defvar my-dired-additional-compression-suffixes
-  '(".7z" ".Z" ".a" ".ace" ".alz" ".arc" ".arj" ".bz" ".bz2" ".cab" ".cpio"
-    ".deb" ".gz" ".jar" ".lha" ".lrz" ".lz" ".lzh" ".lzma" ".lzo" ".rar"
-    ".rpm" ".rz" ".t7z" ".tZ" ".tar" ".tbz" ".tbz2" ".tgz" ".tlz" ".txz"
-    ".tzo" ".war" ".xz" ".zip"))
-(with-eval-after-load 'dired-aux
-  '(progn
-     (cl-loop for suffix in my-dired-additional-compression-suffixes
-           do (add-to-list 'dired-compress-file-suffixes
-                           `(,(concat "\\" suffix "\\'") "" "aunpack")))))
-
-(defun helm-dired-various ()
-  (interactive)
-  (helm '(helm-c-source-dired-various-sort)))
+(custom-set-variables
+ '(insert-directory-program "ls-first-ln-dir") ;lsコマンド指定
+ '(dired-listing-switches "-alFh")      ;diredが使うlsオプションの設定
+ )
 
 (defun dired-jump-to-current ()
   (interactive)
@@ -34,4 +20,5 @@
 (define-key dired-mode-map (kbd "C-o")     'nil)
 (define-key dired-mode-map (kbd "C-t")     'nil)
 (define-key dired-mode-map (kbd "M-s")     'nil)
+(define-key dired-mode-map (kbd "p")       'dired-toggle-marks)
 (define-key dired-mode-map (kbd "t")       'dired-previous-line)
