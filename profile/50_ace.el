58965dbf040acd97a2ee69dcceaa1e8563f4857b
ncaq <ncaq@ncaq.net>
2015-02-28 05:05:23 +0900

    add ace-isearch

--- profile/50_ace.el
+++ profile/50_ace.el
@@ -1,5 +1,7 @@
 ;; -*- lexical-binding: t -*-
 
 (with-eval-after-load 'ace-jump-mode
-  (setq ace-jump-mode-move-keys (append "chtnsbmwvz" nil))
+  (setq ace-jump-mode-move-keys (append "',.paoeu;qjkgcrlhtnsbmwvz" nil))
   )
+
+(global-ace-isearch-mode)
