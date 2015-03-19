84edf5ab8b2cdc47e5940f2d3f30bd74154c44ca
ncaq <ncaq@ncaq.net>
2015-03-19 21:26:39 +0900

    deleted mu4e

--- profile/50_mail.el
+++ profile/50_mail.el
@@ -4,29 +4,6 @@
  '(user-full-name     "ncaq")
  '(user-mail-address  "ncaq@ncaq.net"))
 
-(require 'mu4e)
-(custom-set-variables
- '(mu4e-maildir               "~/.maildir")
- '(mu4e-drafts-folder         "/Drafts")
- '(mu4e-sent-folder           "/Sent")
- '(mu4e-trash-folder          "/Trash")
- 
- '(mu4e-get-mail-command      "offlineimap")
- '(mu4e-html2text-command     "pyhtml2text --body-width=0")
- 
- '(mu4e-refile-folder         nil)
- '(mu4e-update-interval       1000)
- '(mu4e-view-image-max-width  800)
- '(mu4e-view-prefer-html      t)
- '(mu4e-view-show-images      t)
- )
-
-(defun mu4e-jump-to-inbox ()
-  (interactive)
-  (mu4e~headers-jump-to-maildir "/INBOX"))
-
-(define-key mu4e-view-mode-map (kbd "M-q") 'nil)
-
 (require 'smtpmail)
 (custom-set-variables
  '(message-send-mail-function  'smtpmail-send-it)
