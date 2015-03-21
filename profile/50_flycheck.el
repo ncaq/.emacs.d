cc11f810a188ec9979390ddb42ff2d35856d8ff2
ncaq <ncaq@ncaq.net>
2015-03-21 22:10:30 +0900

    50_flycheck.el: eval-after-load -> with-eval-after-load                 add flycheck-list-errors keybind

--- profile/50_flycheck.el
+++ profile/50_flycheck.el
@@ -1,16 +1,15 @@
 ;; -*- lexical-binding: t -*-
 
-(eval-after-load 'flycheck
-  '(progn
-     (global-flycheck-mode 1)
-     
-     (custom-set-variables
-      '(flycheck-display-errors-function nil)                     ;Echoエリアにエラーを表示しない
-      '(flycheck-highlighting-mode nil)                           ;下線があると,_が見えなくなる
-      '(flycheck-idle-change-delay 5)                             ;5秒
-      '(flycheck-indication-mode 'left-fringe)
-      )
+(with-eval-after-load 'flycheck
+  (global-flycheck-mode 1)
+  
+  (custom-set-variables
+   '(flycheck-display-errors-function nil) ; Echoエリアにエラーを表示しない
+   '(flycheck-highlighting-mode nil)    ; 下線があると_が見えなくなる
+   '(flycheck-idle-change-delay 5)      ; 5秒
+   '(flycheck-indication-mode 'left-fringe)
+   )
 
-     (require 'helm-flycheck)
-     (define-key flycheck-mode-map (kbd "C-,") 'helm-flycheck)
-     ))
+  (define-key flycheck-mode-map (kbd "C-,") 'helm-flycheck)
+  (define-key flycheck-mode-map (kbd "C-c ,") 'flycheck-list-errors)
+  )
