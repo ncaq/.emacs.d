d71324f93587b5e482cdaf161d0f0b5fdda07eaf
ncaq <ncaq@ncaq.net>
2015-03-22 09:07:45 +0900

    60_git.el: with-eval-after-load

--- profile/60_git.el
+++ profile/60_git.el
@@ -1,13 +1,13 @@
 ;; -*- lexical-binding: t -*-
-(eval-after-load 'magit
-  '(progn
-    (custom-set-variables
-     '(fill-column 160)
-     '(git-commit-fill-column 160)
-     '(git-commit-mode-hook nil)
-     '(git-commit-summary-max-length 160)
-     '(magit-diff-refine-hunk 'all)
-     )))
+
+(with-eval-after-load 'magit
+  (custom-set-variables
+   '(fill-column 160)
+   '(git-commit-fill-column 160)
+   '(git-commit-mode-hook nil)
+   '(git-commit-summary-max-length 160)
+   '(magit-diff-refine-hunk 'all)
+   ))
 
 (global-git-gutter+-mode 1)
 (custom-set-variables '(git-gutter+:verbosity 2))
