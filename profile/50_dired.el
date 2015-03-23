3f4cdc4f7f0862f6caf5d5bceaa319ebcb98de3a
ncaq <ncaq@ncaq.net>
2015-03-24 01:03:04 +0900

    50_dired.el: remove require

--- profile/50_dired.el
+++ profile/50_dired.el
@@ -1,23 +1,23 @@
 ;; -*- lexical-binding: t -*-
 
-(require 'dired)
-
 (custom-set-variables
  '(dired-listing-switches "-Fhval --group-directories-first")      ;diredが使うlsオプションの設定
  )
 
-(defun dired-jump-to-current ()
-  (interactive)
-  (dired "."))
+(with-eval-after-load 'dired
+  (defun dired-jump-to-current ()
+    (interactive)
+    (dired "."))
 
-(defun dired-disable-M-o()
-  (define-key dired-mode-map (kbd "M-o") 'nil))
+  (defun dired-disable-M-o()
+    (define-key dired-mode-map (kbd "M-o") 'nil))
 
-(add-hook 'dired-mode-hook 'dired-disable-M-o)
-(define-key dired-mode-map (kbd "C-^")     'dired-up-directory)
-(define-key dired-mode-map (kbd "C-c C-p") 'wdired-change-to-wdired-mode)
-(define-key dired-mode-map (kbd "C-o")     'nil)
-(define-key dired-mode-map (kbd "C-t")     'nil)
-(define-key dired-mode-map (kbd "M-s")     'nil)
-(define-key dired-mode-map (kbd "p")       'dired-toggle-marks)
-(define-key dired-mode-map (kbd "t")       'dired-previous-line)
+  (add-hook 'dired-mode-hook 'dired-disable-M-o)
+  (define-key dired-mode-map (kbd "C-^")     'dired-up-directory)
+  (define-key dired-mode-map (kbd "C-c C-p") 'wdired-change-to-wdired-mode)
+  (define-key dired-mode-map (kbd "C-o")     'nil)
+  (define-key dired-mode-map (kbd "C-t")     'nil)
+  (define-key dired-mode-map (kbd "M-s")     'nil)
+  (define-key dired-mode-map (kbd "p")       'dired-toggle-marks)
+  (define-key dired-mode-map (kbd "t")       'dired-previous-line)
+  )
