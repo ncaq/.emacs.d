a2498089094c9ac9d82669a124e38dac296dfc51
ncaq <ncaq@ncaq.net>
2015-03-24 18:12:47 +0900

    removed: scss-compile-at-save

--- profile/30_web.el
+++ profile/30_web.el
@@ -3,10 +3,9 @@
 (custom-set-variables
  '(auto-mode-alist
    (append
-    '(
-      ("\\.html\\'"   . web-mode)
-      ("\\.xhtml\\'"  . web-mode)
-      ("\\.php\\'"  . web-mode)
+    '(("\\.html\\'" web-mode)
+      ("\\.xhtml\\'" . web-mode)
+      ("\\.php\\'" . web-mode)
       )
     auto-mode-alist)))
 
@@ -14,7 +13,4 @@
   (define-key web-mode-map (kbd "M-c") 'web-mode-element-close)
   )
 
-(custom-set-variables
- '(scss-compile-at-save t)
- '(scss-sass-options '("--cache-location" "'/tmp/.sass-cache/'" "--style" "expanded"))
- )
+(custom-set-variables '(scss-sass-options '("--cache-location" "'/tmp/.sass-cache/'" "--style" "expanded")))
