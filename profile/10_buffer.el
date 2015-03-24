960cc1533fc7fb1330768f3ad582f6e838964141
ncaq <ncaq@ncaq.net>
2015-03-24 17:42:01 +0900

    moved: format setting moved to 20_format.el added: profiler-report format setting

--- profile/10_buffer.el
+++ profile/10_buffer.el
@@ -23,26 +23,3 @@
 
 (require 'volatile-highlights)
 (volatile-highlights-mode t)
-
-;;package-listのformat
-(define-derived-mode package-menu-mode tabulated-list-mode "Package Menu"
-  "Major mode for browsing a list of packages.
-Letters do not insert themselves; instead, they are commands.
-\\<package-menu-mode-map>
-\\{package-menu-mode-map}"
-  (setq tabulated-list-format [("Package" 35 package-menu--name-predicate)
-                               ("Version" 15 package-menu--version-predicate)
-                               ("Status"  10 package-menu--status-predicate)
-                               ("Description" 10 package-menu--description-predicate)])
-  (setq tabulated-list-padding 1)
-  (setq tabulated-list-sort-key (cons "Status" nil))
-  (tabulated-list-init-header))
-
-(with-eval-after-load 'ibuffer
-  (custom-set-variables '(ibuffer-formats
-                          '((mark modified read-only " " (name 60 30)
-                                  " " (size 6 -1) " " (mode 16 16) " " filename)
-                            (mark " " (name 60 -1) " " filename))))
-  (define-key ibuffer-mode-map (kbd "C-o") 'nil)
-  (define-key ibuffer-mode-map (kbd "C-t") 'nil)
-  )
