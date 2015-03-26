e7fb89702ac5557e87a36fee9fe3a68c5e66f406
ncaq <ncaq@ncaq.net>
2015-03-27 01:22:30 +0900

    deleted: web-mode

--- profile/30_web.el
+++ profile/30_web.el
@@ -1,16 +1,3 @@
 ;; -*- lexical-binding: t -*-
 
-(custom-set-variables
- '(auto-mode-alist
-   (append
-    '(("\\.html\\'" web-mode)
-      ("\\.xhtml\\'" . web-mode)
-      ("\\.php\\'" . web-mode)
-      )
-    auto-mode-alist)))
-
-(with-eval-after-load 'web-mode
-  (define-key web-mode-map (kbd "M-c") 'web-mode-element-close)
-  )
-
 (custom-set-variables '(scss-sass-options '("--cache-location" "'/tmp/.sass-cache/'" "--style" "expanded")))
