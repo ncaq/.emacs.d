8ba44d126d4be2897a1b38c8adc46143f62f2f09
ncaq <ncaq@ncaq.net>
2015-04-01 08:18:33 +0900

    added: whitespace-auto-cleanup-action-disabled-hooks

--- profile/20_whitespace.el
+++ profile/20_whitespace.el
@@ -1,5 +1,7 @@
 ;; -*- lexical-binding: t -*-
 
+(require 'whitespace)
+
 (custom-set-variables
  '(global-whitespace-mode t)
  '(whitespace-style '(face tabs spaces trailing space-before-tab space-after-tab))
@@ -8,4 +10,21 @@
 
 (set-face-background 'whitespace-space "#073642")
 
-(add-hook 'markdown-mode '(lambda () (setq-local whitespace-action (remove 'auto-cleanup whitespace-action))))
+(defcustom whitespace-auto-cleanup-action-disabled-hooks
+  '(
+    diff-mode-hook
+    markdown-hook
+    )
+  "Called after whitespace-mode auto-cleanup is turned off."
+  :group 'whitespace)
+
+(defun whitespace-auto-cleanup-turn-off ()
+  (interactive)
+  (setq-local whitespace-action (remove 'auto-cleanup whitespace-action))
+  )
+
+(defun add-whitespace-auto-cleanup-action-disable-hook (h)
+  (add-hook h 'whitespace-auto-cleanup-turn-off)
+  )
+
+(mapc 'add-whitespace-auto-cleanup-action-disable-hook 'whitespace-auto-cleanup-action-disabled-modes)
