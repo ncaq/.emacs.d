083283d0784a3f7e5b3b5d0bdf83f19ef8d31005
ncaq <ncaq@ncaq.net>
2015-04-02 12:42:50 +0900

    removed: unneeded with-eval-after-load

--- profile/20_git.el
+++ profile/20_git.el
@@ -1,12 +1,11 @@
 ;; -*- lexical-binding: t -*-
 
-(with-eval-after-load 'magit
-  (custom-set-variables
-   '(fill-column 160)
-   '(git-commit-fill-column 160)
-   '(git-commit-mode-hook nil)
-   '(git-commit-summary-max-length 160)
-   '(magit-diff-refine-hunk 'all)
-   ))
+(custom-set-variables
+ '(fill-column 160)
+ '(git-commit-fill-column 160)
+ '(git-commit-mode-hook nil)
+ '(git-commit-summary-max-length 160)
+ '(magit-diff-refine-hunk 'all)
+ )
 
 (custom-set-variables '(global-git-gutter+-mode t))
