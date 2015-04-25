fbe0d23204fa624154d2ca8f6ee2c6bb34adefdd
ncaq <ncaq@ncaq.net>
2015-04-25 18:46:14 +0900

    added: gtags in haskell. gnu global with pygments.

--- profile/20_tags.el
+++ profile/20_tags.el
@@ -12,24 +12,10 @@
   (define-key helm-gtags-mode-map (kbd "C-M-.") 'helm-gtags-select)
   (define-key helm-gtags-mode-map (kbd "C-c .") 'gtags-visit-rootdir)
   (define-key helm-gtags-mode-map (kbd "M-.")   'helm-gtags-pop-stack)
-
-  ;; monkey patch
-  (defun helm-gtags--read-tagname (type &optional default-tagname)
-    (let ((tagname (helm-gtags--token-at-point type))
-          (prompt (assoc-default type helm-gtags--prompt-alist))
-          (comp-func (assoc-default type helm-gtags-comp-func-alist)))
-      (if (and tagname helm-gtags-use-input-at-cursor)
-          tagname
-        (when (and (not tagname) default-tagname)
-          (setq tagname default-tagname))
-        (when tagname
-          (setq prompt (format "%s(default \"%s\") " prompt tagname)))
-        (let ((completion-ignore-case helm-gtags-ignore-case))
-          (completing-read prompt comp-func nil nil nil
-                           'helm-gtags--completing-history tagname)))))
   )
 
-(add-hook 'c-mode-hook    'helm-gtags-mode)
-(add-hook 'c++-mode-hook  'helm-gtags-mode)
-(add-hook 'java-mode-hook 'helm-gtags-mode)
-(add-hook 'asm-mode-hook  'helm-gtags-mode)
+(add-hook 'c-mode-hook       'helm-gtags-mode)
+(add-hook 'c++-mode-hook     'helm-gtags-mode)
+(add-hook 'java-mode-hook    'helm-gtags-mode)
+(add-hook 'asm-mode-hook     'helm-gtags-mode)
+(add-hook 'haskell-mode-hook 'helm-gtags-mode)
