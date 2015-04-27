0315c8bf10f5d6385be447705309dc72d8ee907b
ncaq <ncaq@ncaq.net>
2015-04-27 13:50:56 +0900

    changed: auto-mode-alist configure by add-to-list

--- profile/30_extension.el
+++ profile/30_extension.el
@@ -2,22 +2,18 @@
 
 (autoload 'R-mode "ess-site" "Emacs Speaks Statistics mode" t) ;http://www.okada.jp.org/RWiki/?ESS
 
+(add-to-list 'auto-mode-alist '("\\.R\\'"          . R-mode))
+(add-to-list 'auto-mode-alist '("\\.license\\'"    . conf-mode))
+(add-to-list 'auto-mode-alist '("\\.mask\\'"       . conf-mode))
+(add-to-list 'auto-mode-alist '("\\.r\\'"          . R-mode))
+(add-to-list 'auto-mode-alist '("\\.ssh/config\\'" . ssh-config-mode))
+(add-to-list 'auto-mode-alist '("\\.unmask\\'"     . conf-mode))
+(add-to-list 'auto-mode-alist '("\\.use\\'"        . conf-mode))
+(add-to-list 'auto-mode-alist '("\\.zsh\\'"        . shell-script-mode))
+(add-to-list 'auto-mode-alist '("nginx.conf\\'"    . nginx-mode))
+(add-to-list 'auto-mode-alist '("sshd?_config\\'"  . ssh-config-mode))
+
 (custom-set-variables
- '(auto-mode-alist
-   (append
-    '(
-      ("\\.R\\'"          . R-mode)
-      ("\\.license\\'"    . conf-mode)
-      ("\\.mask\\'"       . conf-mode)
-      ("\\.r\\'"          . R-mode)
-      ("\\.ssh/config\\'" . ssh-config-mode)
-      ("\\.unmask\\'"     . conf-mode)
-      ("\\.use\\'"        . conf-mode)
-      ("\\.zsh\\'"        . shell-script-mode)
-      ("nginx.conf\\'"    . nginx-mode)
-      ("sshd?_config\\'"  . ssh-config-mode)
-      )
-    auto-mode-alist))
  '(scheme-program-name "gosh")
  '(scss-sass-options '("--cache-location" "'/tmp/.sass-cache/'" "--style" "expanded"))
  )
