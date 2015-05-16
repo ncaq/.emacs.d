37fdc45e2d9c67e755251c98df853469ee588c0b
ncaq <ncaq@ncaq.net>
2015-05-16 15:42:36 +0900

    added: magit dvorak keybinding

--- profile/20_git.el
+++ profile/20_git.el
@@ -10,3 +10,19 @@
  )
 
 (setq magit-last-seen-setup-instructions "1.4.0")
+
+(with-eval-after-load 'magit
+  (define-key magit-mode-map (kbd "p") 'magit-key-mode-popup-tagging)
+  (define-key magit-mode-map (kbd "t") 'magit-goto-previous-section)
+
+  (define-key magit-mode-map (kbd "M-h")   'nil)
+  (define-key magit-mode-map (kbd "M-t")   'nil)
+  (define-key magit-mode-map (kbd "M-n")   'nil)
+  (define-key magit-mode-map (kbd "M-s")   'nil)
+  (define-key magit-mode-map (kbd "M-p")   'nil)
+
+  (define-key magit-mode-map (kbd "C-M-h") 'nil)
+  (define-key magit-mode-map (kbd "C-M-t") 'magit-goto-previous-sibling-section)
+  (define-key magit-mode-map (kbd "C-M-n") 'magit-goto-next-sibling-section)
+  (define-key magit-mode-map (kbd "C-M-s") 'nil)
+  )
