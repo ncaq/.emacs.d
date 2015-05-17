7db9a2a46e7c927ca377253ca7af2a02e5a01435
ncaq <ncaq@ncaq.net>
2015-05-17 14:36:53 +0900

    modified: eww keybinding

--- profile/20_eww.el
+++ profile/20_eww.el
@@ -5,15 +5,15 @@
    '(eww-search-prefix "https://www.google.co.jp/search?q="))
 
   (define-key eww-mode-map (kbd "h") 'backward-char)
+  (define-key eww-mode-map (kbd "t") 'previous-line)
   (define-key eww-mode-map (kbd "n") 'next-line)
   (define-key eww-mode-map (kbd "s") 'forward-char)
-  (define-key eww-mode-map (kbd "t") 'previous-line)
 
   (define-key eww-mode-map (kbd "H") 'eww-back-url)
   (define-key eww-mode-map (kbd "S") 'eww-forward-url)
 
-  (define-key eww-mode-map (kbd "b") 'eww-history-browse)
   (define-key eww-mode-map (kbd "c") 'eww-browse-with-external-browser)
+  (define-key eww-mode-map (kbd "d") 'eww-history-browse)
   (define-key eww-mode-map (kbd "i") 'eww)
   (define-key eww-mode-map (kbd "m") 'ace-link-eww)
 
