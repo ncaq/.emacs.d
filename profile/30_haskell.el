66241ec0aa12dbfd8dcd6318e8e5dcc1eb2f6941
ncaq <ncaq@ncaq.net>
2015-05-30 21:31:41 +0900

    deleted: haskell-indentation setting

--- profile/30_haskell.el
+++ profile/30_haskell.el
@@ -1,16 +1,12 @@
 ;; -*- lexical-binding: t -*-
 
 (with-eval-after-load 'haskell-mode
-  (custom-set-variables
-   '(haskell-indentation-ifte-offset 4)
-   '(haskell-indentation-layout-offset 4)
-   '(haskell-indentation-left-offset 4)
-   '(haskell-stylish-on-save t)
-   )
+  (custom-set-variables '(haskell-stylish-on-save t))
   (add-hook 'haskell-mode-hook 'haskell-indentation-mode)
 
-  (add-to-list 'ac-modes 'haskell-interactive-mode)
-  (add-hook 'inferior-haskell-mode-hook 'ac-haskell-process-setup)
+  (with-eval-after-load 'auto-complete
+    (add-to-list 'ac-modes 'haskell-interactive-mode)
+    (add-hook 'inferior-haskell-mode-hook 'ac-haskell-process-setup))
 
   (with-eval-after-load 'flycheck
     (add-hook 'flycheck-mode-hook 'flycheck-haskell-setup))
