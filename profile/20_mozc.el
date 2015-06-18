599362d1e57cfeee1c83cd91c4e41f845f730aeb
ncaq <ncaq@ncaq.net>
2015-06-18 09:48:45 +0900

    deleted: mozc-popup, because it was heavy

--- profile/20_mozc.el
+++ profile/20_mozc.el
@@ -1,11 +1,10 @@
 ;; -*- lexical-binding: t -*-
 
 (require 'mozc-im)
-(require 'mozc-popup)
 
 (custom-set-variables
  '(default-input-method "japanese-mozc-im")
- '(mozc-candidate-style 'popup)
+ '(mozc-candidate-style 'echo-area)
  )
 
 (defun cursor-color-toggle ()
