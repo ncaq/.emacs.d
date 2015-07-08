1a2d029149c987745b341979159fc48df0ff66e6
ncaq <ncaq@ncaq.net>
2015-07-08 21:29:21 +0900

    added: robe setting

--- profile/30_ruby.el
+++ profile/30_ruby.el
@@ -3,12 +3,15 @@
 (add-to-list 'auto-mode-alist '("\\.rb\\'" . enh-ruby-mode))
 
 (custom-set-variables
+ '(ac-modes (append '(enh-ruby-mode inf-ruby-mode) ac-modes))
  '(inf-ruby-default-implementation "pry")
  '(inf-ruby-eval-binding "Pry.toplevel_binding")
  )
 
-(with-eval-after-load 'auto-complete (add-to-list 'ac-modes 'inf-ruby-mode))
 (add-hook 'inf-ruby-mode-hook 'ac-inf-ruby-enable)
+
+(add-hook 'enh-ruby-mode-hook 'robe-mode)
+(add-hook 'robe-mode-hook 'ac-robe-setup)
 
 (with-eval-after-load 'enh-ruby-mode
   (define-key enh-ruby-mode-map "C-j" 'nil)
