1c913303033cb8874e02c46d2dc2f49ccac12bc9
ncaq <ncaq@ncaq.net>
2015-08-11 19:31:26 +0900

    deleted: enh-ruby-mode

--- profile/90_ruby.el
+++ profile/90_ruby.el
@@ -1,19 +1,14 @@
 ;; -*- lexical-binding: t -*-
 
-(add-to-list 'auto-mode-alist '("\\.rb\\'" . enh-ruby-mode))
-
 (custom-set-variables
- '(ac-modes (append '(enh-ruby-mode inf-ruby-mode) ac-modes))
+ '(ac-modes (append '(inf-ruby-mode) ac-modes))
  '(inf-ruby-default-implementation "pry")
  '(inf-ruby-eval-binding "Pry.toplevel_binding")
  )
 
 (add-hook 'inf-ruby-mode-hook 'ac-inf-ruby-enable)
 
-(add-hook 'enh-ruby-mode-hook 'robe-mode)
+(add-hook 'ruby-mode-hook 'robe-mode)
 (add-hook 'robe-mode-hook 'ac-robe-setup)
 
-(with-eval-after-load 'enh-ruby-mode
-  (ncaq-set-key enh-ruby-mode-map)
-  (define-key enh-ruby-mode-map "C-j" 'nil)
-  )
+(with-eval-after-load 'ruby-mode (ncaq-set-key ruby-mode-map))
