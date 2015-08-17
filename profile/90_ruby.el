277eac1e83895114410913bd0f65dba1db95262b
ncaq <ncaq@ncaq.net>
2015-08-17 20:23:38 +0900

    deleted: robe.

--- profile/90_ruby.el
+++ profile/90_ruby.el
@@ -6,9 +6,4 @@
  '(inf-ruby-eval-binding "Pry.toplevel_binding")
  )
 
-(add-hook 'inf-ruby-mode-hook 'ac-inf-ruby-enable)
-
-(add-hook 'ruby-mode-hook 'robe-mode)
-(add-hook 'robe-mode-hook 'ac-robe-setup)
-
 (with-eval-after-load 'ruby-mode (ncaq-set-key ruby-mode-map))
