bf9f5af48d178c568c62b63174f53670f260144f
ncaq <ncaq@ncaq.net>
2015-08-17 22:04:40 +0900

    changed: haskell-indent offset to 4.

--- profile/90_haskell.el
+++ profile/90_haskell.el
@@ -3,13 +3,11 @@
 (custom-set-variables
  '(ac-modes (append '(haskell-mode inferior-haskell-mode haskell-interactive-mode) ac-modes))
  '(haskell-stylish-on-save t)
- '(safe-local-variable-values (append '((haskell-process-use-ghci . t)
-                                        (haskell-indent-spaces . 4))
-                                      safe-local-variable-values))
+ '(haskell-indent-after-keywords '("where" "of" "do" "mdo" "rec" "in" "{" "if" "then" "else" "let"))
  )
 
 (with-eval-after-load 'haskell-mode
-  (add-hook 'haskell-mode-hook 'haskell-indentation-mode)
+  (add-hook 'haskell-mode-hook 'haskell-indent-mode)
 
   (with-eval-after-load 'auto-complete
     (add-hook 'inferior-haskell-mode-hook 'ac-haskell-process-setup))
@@ -17,7 +15,7 @@
   (with-eval-after-load 'flycheck
     (add-hook 'flycheck-mode-hook 'flycheck-haskell-setup))
 
-  (defun haskell-mode-stylish-buffer-and-save-buffer()
+  (defun haskell-mode-stylish-buffer-and-save-buffer ()
     (interactive)
     (save-buffer)
     (haskell-mode-stylish-buffer))
