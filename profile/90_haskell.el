dea399ed6792cbc3615d79cceec97708ed0f5d60
ncaq <ncaq@ncaq.net>
2015-08-19 20:06:12 +0900

    changed: haskell "where" indent 4 -> 2.

--- profile/90_haskell.el
+++ profile/90_haskell.el
@@ -7,8 +7,8 @@
  '(haskell-indentation-layout-offset 4)
  '(haskell-indentation-left-offset 4)
  '(haskell-indentation-starter-offset 4)
- '(haskell-indentation-where-post-offset 4)
- '(haskell-indentation-where-pre-offset 4)
+ '(haskell-indentation-where-post-offset 2)
+ '(haskell-indentation-where-pre-offset 2)
  '(hamlet/basic-offset 4)
  )
 
