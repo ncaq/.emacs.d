3074b312c7bbb970b9d6a51b4bf51e6fc0047d8e
ncaq <ncaq@ncaq.net>
2015-08-21 21:28:58 +0900

    changed: helm keybinds

--- profile/60_helm.el
+++ profile/60_helm.el
@@ -1,40 +1,28 @@
 ;; -*- lexical-binding: t -*-
 
-(require 'helm)
-(require 'helm-files)
-
-(helm-mode 1)
-(helm-descbinds-mode 1)
+(require 'helm-config)
 
 (custom-set-variables
  '(helm-boring-buffer-regexp-list (append '("\\*tramp" "\\*magit-process\\*") helm-boring-buffer-regexp-list))
  '(helm-buffer-max-len-mode 25)                  ;モードを短縮する基準
  '(helm-buffer-max-length 50)                    ;デフォルトはファイル名を短縮する区切りが20
  '(helm-delete-minibuffer-contents-from-point t) ;kill-line sim
+ '(helm-descbinds-mode t)
  '(helm-samewindow t)                            ;ウインドウ全体に表示
  )
 
-(defvar helm-for-files-lite-preferred-list '(helm-source-buffers-list helm-source-recentf helm-source-file-cache))
-
-(defun helm-for-files-lite ()
-  (interactive)
-  (unless helm-source-buffers-list
-    (setq helm-source-buffers-list
-          (helm-make-source "Buffers" 'helm-source-buffers)))
-  (let ((helm-ff-transformer-show-only-basename nil))
-    (helm :sources helm-for-files-lite-preferred-list :buffer "*helm for files*")))
+(helm-mode 1)
 
 (swap-set-key helm-map '(("C-t" . "C-p")
                          ("C-s" . "C-f")))
-(define-key helm-map (kbd "C-h") 'nil)
-(define-key helm-map (kbd "M-b") 'nil)
-(define-key helm-map (kbd "C-M-b") 'nil)
 
-(define-key helm-buffer-map (kbd "C-s") 'nil)
-(define-key helm-find-files-map (kbd "C-s") 'nil)
-(define-key helm-generic-files-map (kbd "C-s") 'nil)
+(define-key helm-buffer-map        (kbd "C-s")   'nil)
+(define-key helm-find-files-map    (kbd "C-s")   'nil)
+(define-key helm-generic-files-map (kbd "C-s")   'nil)
+(define-key helm-map               (kbd "C-M-b") 'nil)
+(define-key helm-map               (kbd "C-h")   'nil)
+(define-key helm-map               (kbd "M-b")   'nil)
+(define-key helm-map               (kbd "M-s")   'nil)
 
 (define-key helm-map (kbd "<tab>") 'helm-select-action)
 (define-key helm-map (kbd "C-b") 'backward-delete-char-untabify)
-(define-key helm-find-files-map (kbd "C-'") 'helm-execute-persistent-action)
-(define-key helm-read-file-map  (kbd "C-'") 'helm-execute-persistent-action)
