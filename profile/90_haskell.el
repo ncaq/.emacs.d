1b6b382ef47e86dfc83c1a41c8b13c74b8f476a8
ncaq <ncaq@ncaq.net>
2015-09-03 19:34:36 +0900

    added: use haskell-mode function

--- profile/90_haskell.el
+++ profile/90_haskell.el
@@ -2,35 +2,38 @@
 
 (custom-set-variables
  '(ac-modes (append '(haskell-mode inferior-haskell-mode haskell-interactive-mode) ac-modes))
- '(haskell-stylish-on-save t)
+ '(hamlet/basic-offset 4)
  '(haskell-indentation-ifte-offset 4)
  '(haskell-indentation-layout-offset 4)
  '(haskell-indentation-left-offset 4)
  '(haskell-indentation-starter-offset 4)
  '(haskell-indentation-where-post-offset 2)
  '(haskell-indentation-where-pre-offset 2)
- '(hamlet/basic-offset 4)
+ '(haskell-process-suggest-language-pragmas nil)
+ '(haskell-stylish-on-save t)
  )
 
 (with-eval-after-load 'haskell-mode
   (add-hook 'haskell-mode-hook 'haskell-indentation-mode)
-
-  (with-eval-after-load 'auto-complete
-    (add-hook 'inferior-haskell-mode-hook 'ac-haskell-process-setup))
-
-  (with-eval-after-load 'flycheck
-    (add-hook 'flycheck-mode-hook 'flycheck-haskell-setup))
-
-  (with-eval-after-load 'haskell-interactive-mode
-    (ncaq-set-key haskell-interactive-mode-map))
+  (add-hook 'haskell-mode-hook 'interactive-haskell-mode)
+  (add-hook 'haskell-mode-hook 'haskell-doc-mode)
+  (add-hook 'haskell-mode-hook 'ac-haskell-process-setup)
+  (add-hook 'inferior-haskell-mode-hook 'ac-haskell-process-setup)
+  (add-hook 'flycheck-mode-hook 'flycheck-haskell-setup)
 
   (defun haskell-mode-stylish-buffer-and-save-buffer ()
     (interactive)
     (save-buffer)
     (haskell-mode-stylish-buffer))
 
-  (define-key haskell-mode-map [remap indent-whole-buffer] 'haskell-mode-stylish-buffer-and-save-buffer)
+  (defun haskell-process-load-or-reload-and-switch ()
+    (interactive)
+    (haskell-process-load-or-reload)
+    (switch-to-buffer (haskell-process)))
+
+  (with-eval-after-load 'haskell-interactive-mode (ncaq-set-key haskell-interactive-mode-map))
+
   (define-key haskell-mode-map (kbd "C-c C-d") 'hayoo)
-  (define-key haskell-mode-map (kbd "C-c C-l") 'inferior-haskell-load-file)
-  (define-key haskell-mode-map (kbd "C-c C-r") 'haskell-process-load-or-reload)
+  (define-key haskell-mode-map (kbd "C-c C-l") 'haskell-process-load-or-reload-and-switch)
+  (define-key haskell-mode-map [remap indent-whole-buffer] 'haskell-mode-stylish-buffer-and-save-buffer)
   )
