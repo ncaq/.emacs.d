fd2d2b9997f0af7d99ec983167adfed66da81a31
ncaq <ncaq@ncaq.net>
2015-09-28 01:16:04 +0900

    added: generic-x

--- profile/90_extension.el
+++ profile/90_extension.el
@@ -5,11 +5,10 @@
 (add-to-list 'auto-mode-alist '("\\.R\\'"          . R-mode))
 (add-to-list 'auto-mode-alist '("\\.jsx\\'"        . jsx-mode))
 (add-to-list 'auto-mode-alist '("\\.license\\'"    . conf-mode))
-(add-to-list 'auto-mode-alist '("\\.mask\\'"       . conf-mode))
 (add-to-list 'auto-mode-alist '("\\.r\\'"          . R-mode))
 (add-to-list 'auto-mode-alist '("\\.ssh/config\\'" . ssh-config-mode))
-(add-to-list 'auto-mode-alist '("\\.unmask\\'"     . conf-mode))
 (add-to-list 'auto-mode-alist '("\\.use\\'"        . conf-mode))
 (add-to-list 'auto-mode-alist '("\\.zsh\\'"        . shell-script-mode))
-(add-to-list 'auto-mode-alist '("nginx.conf\\'"    . nginx-mode))
 (add-to-list 'auto-mode-alist '("sshd?_config\\'"  . ssh-config-mode))
+
+(require 'generic-x)
