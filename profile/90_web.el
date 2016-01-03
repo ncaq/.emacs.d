c16b26c9cfda6cd80c9705752f07c1a60e2d805d
ncaq <ncaq@ncaq.net>
2016-01-03 15:50:20 +0900

    added: web-mode settings

---
+++ profile/90_web.el
@@ -0,0 +1,14 @@
+;; -*- lexical-binding: t -*-
+
+(custom-set-variables '(safe-local-variable-values '((web-mode-markup-indent-offset . 2))))
+
+(add-to-list 'auto-mode-alist '("\\.[agj]sp\\'"   . web-mode))
+(add-to-list 'auto-mode-alist '("\\.as[cp]x\\'"   . web-mode))
+(add-to-list 'auto-mode-alist '("\\.djhtml\\'"    . web-mode))
+(add-to-list 'auto-mode-alist '("\\.erb\\'"       . web-mode))
+(add-to-list 'auto-mode-alist '("\\.mustache\\'"  . web-mode))
+(add-to-list 'auto-mode-alist '("\\.phtml\\'"     . web-mode))
+(add-to-list 'auto-mode-alist '("\\.tpl\\.php\\'" . web-mode))
+
+(with-eval-after-load 'web-mode
+  (sp-local-pair '(web-mode) "<" ">" :actions :rem))
