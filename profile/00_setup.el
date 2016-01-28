a8e361427f354a348c13f7a58ac78b7e9b6c11d7
ncaq <ncaq@ncaq.net>
2016-01-28 12:21:35 +0900

    changed: disable exec-path-from-shell warning

--- profile/00_setup.el
+++ profile/00_setup.el
@@ -1,12 +1,12 @@
 ;; -*- lexical-binding: t -*-
 
 (custom-set-variables
+ '(exec-path-from-shell-check-startup-files nil)
  '(gc-cons-percentage 10)               ; 10%以上作成したらGC
  '(gc-cons-threshold 16000000)          ; 16MB以上作成したらGC
  '(inhibit-startup-screen t)            ; 起動時の画面無効
  )
 
-(require 'exec-path-from-shell)
 (exec-path-from-shell-initialize)
 
 (require 'server)
