837a8093d4c8dc6f8e7700dc985261a3b3f58f73
ncaq <ncaq@ncaq.net>
2016-08-25 23:27:22 +0900

    deleted: 50_history: setting that i forget reason

--- profile/50_history.el
+++ profile/50_history.el
@@ -4,51 +4,44 @@
  '(make-backup-files t)              ;バックアップファイルを作成する。
  '(version-control t)                ;複数バックアップ
  '(delete-old-versions t)            ;askだといちいち聞いてくる
- '(kept-new-versions 50)             ;backupに新しいものをいくつ残すか
- '(kept-old-versions 50)             ;backupに古いものをいくつ残すか
+ '(kept-new-versions 20)             ;backupに新しいものをいくつ残すか
+ '(kept-old-versions 20)             ;backupに古いものをいくつ残すか
  '(backup-directory-alist `(("" . ,(concat user-emacs-directory "file-backup/"))))
 
- '(auto-save-file-name-transforms `((".*" ,temporary-file-directory t)))
  '(tramp-auto-save-directory temporary-file-directory)
- '(auto-save-list-file-prefix (concat temporary-file-directory "auto-save-list/.saves-"))
 
  '(message-log-max 100000)
 
  '(savehist-mode t)
- '(savehist-ignored-variables '(file-name-history))
 
  '(desktop-save-mode t)
  '(desktop-restore-frames nil)
  )
 
 (defun recentf-setup ()
+  (require 'recentf-ext)
+  (require 'recentf-remove-sudo-tramp-prefix)
   (custom-set-variables
-   '(recentf-max-saved-items 1000)
+   '(recentf-max-saved-items (* recentf-max-saved-items 10))
 
    '(helm-ff-file-name-history-use-recentf t)
-   '(recentf-auto-cleanup 300)
+   '(recentf-auto-cleanup (* 15 60))
 
    '(recentf-exclude '(
                        "\\.elc$"
                        "\\.o$"
-                       "autoloads\\.el"
                        "~$"
-
-                       "COMMIT_EDITMSG$"
-                       "GPATH$"
-                       "GRTAGS$"
-                       "GTAGS$"
-                       "TAGS$"
 
                        "\\.file-backup/"
                        "\\.undo-tree/"
-                       ))
-   )
-  (require 'recentf-ext)
-  (require 'recentf-remove-sudo-tramp-prefix)
-  )
+
+                       "EDITMSG"
+                       "PATH"
+                       "TAGS"
+                       "autoloads"
+                       ))))
 
 (add-hook 'after-init-hook 'recentf-setup)
 
-(setq-default save-place t)
 (require 'saveplace)
+(setq-default save-place t)
