826317269bf192aa89c0efb8a7e4d66080ad706a
ncaq <ncaq@ncaq.net>
2016-08-25 23:13:28 +0900

    changed: 90_extension: auto-mode-alist

--- profile/90_extension.el
+++ profile/90_extension.el
@@ -1,10 +1,16 @@
 ;; -*- lexical-binding: t -*-
 
-(add-to-list 'auto-mode-alist '("\\.jsx\\'"        . jsx-mode))
-(add-to-list 'auto-mode-alist '("\\.license\\'"    . conf-mode))
-(add-to-list 'auto-mode-alist '("\\.ssh/config\\'" . ssh-config-mode))
-(add-to-list 'auto-mode-alist '("\\.use\\'"        . conf-mode))
-(add-to-list 'auto-mode-alist '("\\.zsh\\'"        . shell-script-mode))
-(add-to-list 'auto-mode-alist '("sshd?_config\\'"  . ssh-config-mode))
-
 (require 'generic-x)
+
+(add-to-list 'auto-mode-alist '("\\.accept_keywords$" . conf-space-mode))
+(add-to-list 'auto-mode-alist '("\\.keywords$" . conf-space-mode))
+(add-to-list 'auto-mode-alist '("\\.license$" . conf-space-mode))
+(add-to-list 'auto-mode-alist '("\\.mask$" . conf-space-mode))
+(add-to-list 'auto-mode-alist '("\\.unmask$" . conf-space-mode))
+(add-to-list 'auto-mode-alist '("\\.use$" . conf-space-mode))
+
+(add-to-list 'auto-mode-alist '("\\.ssh/config$" . ssh-config-mode))
+(add-to-list 'auto-mode-alist '("sshd?_config$" . ssh-config-mode))
+
+(add-to-list 'auto-mode-alist '("\\.jsx$" . jsx-mode))
+(add-to-list 'auto-mode-alist '("\\.zsh$" . shell-script-mode))
