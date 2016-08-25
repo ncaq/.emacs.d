7eb6170bf795816058410a96de28b777f6527350
ncaq <ncaq@ncaq.net>
2016-08-25 22:55:34 +0900

    deleted: ess

--- profile/90_extension.el
+++ profile/90_extension.el
@@ -1,11 +1,7 @@
 ;; -*- lexical-binding: t -*-
 
-(autoload 'R-mode "ess-site" "Emacs Speaks Statistics mode" t) ;http://www.okada.jp.org/RWiki/?ESS
-
-(add-to-list 'auto-mode-alist '("\\.R\\'"          . R-mode))
 (add-to-list 'auto-mode-alist '("\\.jsx\\'"        . jsx-mode))
 (add-to-list 'auto-mode-alist '("\\.license\\'"    . conf-mode))
-(add-to-list 'auto-mode-alist '("\\.r\\'"          . R-mode))
 (add-to-list 'auto-mode-alist '("\\.ssh/config\\'" . ssh-config-mode))
 (add-to-list 'auto-mode-alist '("\\.use\\'"        . conf-mode))
 (add-to-list 'auto-mode-alist '("\\.zsh\\'"        . shell-script-mode))
