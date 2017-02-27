8370d161c0688c63e6b23076f458a3216e695ecb
ncaq <ncaq@ncaq.net>
2017-02-28 03:46:48 +0900

    changed: web-mode support tsx

--- profile/90_web.el
+++ profile/90_web.el
@@ -9,7 +9,7 @@
 (add-to-list 'auto-mode-alist '("\\.mustache\\'"  . web-mode))
 (add-to-list 'auto-mode-alist '("\\.phtml\\'"     . web-mode))
 (add-to-list 'auto-mode-alist '("\\.tpl\\.php\\'" . web-mode))
-(add-to-list 'auto-mode-alist '("\\.tsx\\'"       . typescript-mode))
+(add-to-list 'auto-mode-alist '("\\.tsx\\'"       . web-mode))
 
 (with-eval-after-load 'web-mode
   (sp-local-pair '(web-mode) "<" ">" :actions :rem))
@@ -18,5 +18,3 @@
   (setq-local js-indent-level 2))
 
 (add-hook 'json-mode-hook 'json-ncaq-setup)
-
-(add-hook 'typescript-mode-hook (lambda () (tide-setup)))
