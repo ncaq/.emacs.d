d63cd88d7fdfe9375d6e218e4022e4f33ce6c042
ncaq <ncaq@ncaq.net>
2017-06-06 09:31:50 +0900

    deleted: 使っていない設定を削除

--- profile/90_extension.el
+++ profile/90_extension.el
@@ -12,5 +12,4 @@
 (add-to-list 'auto-mode-alist '("\\.ssh/config$" . ssh-config-mode))
 (add-to-list 'auto-mode-alist '("sshd?_config$" . ssh-config-mode))
 
-(add-to-list 'auto-mode-alist '("\\.jsx$" . jsx-mode))
 (add-to-list 'auto-mode-alist '("\\.zsh$" . shell-script-mode))
