0ded2968b62d14736c92c8747feb8c57d78a7f69
ncaq <ncaq@ncaq.net>
2017-06-06 09:20:43 +0900

    deleted: maxima.el

--- profile/60_etc.el
+++ profile/60_etc.el
@@ -1,22 +1,23 @@
 ;; -*- lexical-binding: t -*-
 
 (custom-set-variables
- '(blink-matching-paren nil)                ;括弧移動無効
- '(delete-by-moving-to-trash t)             ;ごみ箱を有効
- '(diff-switches "-u")                      ;diffをunifitedモードで
+ '(blink-matching-paren nil)            ;括弧移動無効
+ '(delete-by-moving-to-trash t)         ;ごみ箱を有効
+ '(diff-switches "-u")                  ;diffをunifitedモードで
  '(disabled-command-function nil)
  '(google-translate-default-source-language "en")
  '(google-translate-default-target-language "ja")
  '(graphviz-dot-auto-indent-on-semi nil)
+ '(imaxima-fnt-size "LARGE")            ;maximaの文字を大きく
  '(indent-tabs-mode nil)                    ;インデントをスペースで行う
  '(read-buffer-completion-ignore-case t)    ;大文字と小文字を区別しない バッファ名
  '(read-file-name-completion-ignore-case t) ;大文字と小文字を区別しない ファイル名
  '(require-final-newline t)                 ;POSIX改行
  '(scheme-program-name "gosh")
- '(scroll-step 1)                           ;最下段までScrollしてもカーソルを中心に戻さない
+ '(scroll-step 1)                       ;最下段までScrollしてもカーソルを中心に戻さない
  '(skype--my-user-handle "ncaq__")
- '(warning-minimum-level :error)            ;quiet waring
- '(x-select-enable-clipboard t)             ;クリップボードをX11と共有
+ '(warning-minimum-level :error)        ;quiet waring
+ '(x-select-enable-clipboard t)         ;クリップボードをX11と共有
  )
 
 (add-hook 'after-save-hook 'executable-make-buffer-file-executable-if-script-p) ;スクリプトに実行権限付加
--- profile/90_maxima.el
+++
@@ -1,6 +0,0 @@
-;; -*- lexical-binding: t -*-
-
-(custom-set-variables
- '(imaxima-fnt-size "LARGE")
- '(imaxima-latex-preamble "\\usepackage{concrete}")
- )
