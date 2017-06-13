a5e57f6d2d0c1fe1b55108b5ab34a22641cc15bf
ncaq <ncaq@ncaq.net>
2017-06-13 16:02:45 +0900

    added: (markdown-hide-urls nil)

--- profile/90_markdown.el
+++ profile/90_markdown.el
@@ -4,7 +4,10 @@
 
 (with-eval-after-load 'markdown-mode
   (ncaq-set-key markdown-mode-map)
-  (custom-set-variables '(markdown-command "pandoc"))
+  (custom-set-variables
+   '(markdown-command "pandoc")
+   '(markdown-hide-urls nil)
+   )
   (add-hook 'markdown-mode-hook 'pandoc-mode)
   (add-hook 'pandoc-mode-hook 'pandoc-load-default-settings)
   )
