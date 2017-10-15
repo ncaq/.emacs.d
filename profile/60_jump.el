b9681637604a17b5129a8134fe08bbf6ad43b281
ncaq <ncaq@ncaq.net>
2017-10-15 21:41:55 +0900

    added: helm-etags-select keybind

--- profile/60_jump.el
+++ profile/60_jump.el
@@ -1,5 +1,15 @@
 ;; -*- lexical-binding: t -*-
 
+(defun etags-dir? ()
+  (locate-dominating-file default-directory "TAGS"))
+
+(defun etags-mode-when-etags-dir ()
+  (when (etags-dir?)
+    (local-set-key (kbd "C-.") 'helm-etags-select)
+    ))
+
+(add-hook 'find-file-hook 'etags-mode-when-etags-dir)
+
 (autoload 'helm-ag--project-root "helm-ag")
 (defun helm-do-ag-project-root-or-normal ()
   (interactive)
