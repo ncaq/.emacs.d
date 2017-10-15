2ebe2856cd6fcde6d4dc55cb6131ad9356552db4
ncaq <ncaq@ncaq.net>
2017-10-15 21:22:59 +0900

    deleted: helm-gtags

--- profile/60_jump.el
+++ profile/60_jump.el
@@ -1,24 +1,5 @@
 ;; -*- lexical-binding: t -*-
 
-(custom-set-variables
- '(helm-gtags-auto-update t)
- '(helm-gtags-ignore-case t)
- )
-
-(defun gtags-dir? ()
-  (locate-dominating-file default-directory "GTAGS"))
-
-(defun helm-gtags-mode-when-gtags-dir ()
-  (when (gtags-dir?)
-    (helm-gtags-mode t)))
-
-(add-hook 'find-file-hook 'helm-gtags-mode-when-gtags-dir)
-
-(with-eval-after-load 'helm-gtags
-  (define-key helm-gtags-mode-map (kbd "M-.") 'helm-gtags-dwim)
-  (define-key helm-gtags-mode-map [remap pop-tag-mark] 'helm-gtags-pop-stack)
-  )
-
 (autoload 'helm-ag--project-root "helm-ag")
 (defun helm-do-ag-project-root-or-normal ()
   (interactive)
