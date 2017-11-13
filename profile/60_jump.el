231fdc471935f4ec5c6c200b7c9bcd0737a23ab5
ncaq <ncaq@ncaq.net>
2017-11-13 19:42:29 +0900

    added: helm-gtags settings

--- profile/60_jump.el
+++ profile/60_jump.el
@@ -1,14 +1,35 @@
 ;; -*- lexical-binding: t -*-
 
+(custom-set-variables
+ '(helm-gtags-auto-update t)
+ '(helm-gtags-ignore-case t)
+ )
+
+(defun gtags-dir? ()
+  (locate-dominating-file default-directory "GTAGS"))
+
+(defun helm-gtags-mode-when-gtags-dir ()
+  (when (gtags-dir?)
+    (helm-gtags-mode t)))
+
+(add-hook 'find-file-hook 'helm-gtags-mode-when-gtags-dir)
+
+(with-eval-after-load 'helm-gtags
+  (define-key helm-gtags-mode-map (kbd "C-.") 'helm-gtags-select)
+  (define-key helm-gtags-mode-map (kbd "C->") 'helm-gtags-find-symbol)
+  (define-key helm-gtags-mode-map (kbd "M-.") 'helm-gtags-dwim)
+  (define-key helm-gtags-mode-map [remap pop-tag-mark] 'helm-gtags-pop-stack)
+  )
+
 (defun etags-dir? ()
   (locate-dominating-file default-directory "TAGS"))
 
-(defun etags-mode-when-etags-dir ()
+(defun helm-etags-mode-when-etags-dir ()
   (when (etags-dir?)
     (local-set-key (kbd "C-.") 'helm-etags-select)
     ))
 
-(add-hook 'find-file-hook 'etags-mode-when-etags-dir)
+(add-hook 'find-file-hook 'helm-etags-mode-when-etags-dir)
 
 (autoload 'helm-ag--project-root "helm-ag")
 (defun helm-do-ag-project-root-or-normal ()
