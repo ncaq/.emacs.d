051f5f0412f9bb27e4189bd633398c78462538d0
ncaq <ncaq@ncaq.net>
2017-12-09 11:05:16 +0900

    added: jsx to web-mode

--- profile/90_web.el
+++ profile/90_web.el
@@ -11,6 +11,7 @@
 (add-to-list 'auto-mode-alist '("\\.[agj]sp\\'"  . web-mode))
 (add-to-list 'auto-mode-alist '("\\.as[cp]x\\'"  . web-mode))
 (add-to-list 'auto-mode-alist '("\\.erb\\'"      . web-mode))
+(add-to-list 'auto-mode-alist '("\\.jsx\\'"      . web-mode))
 (add-to-list 'auto-mode-alist '("\\.mustache\\'" . web-mode))
 (add-to-list 'auto-mode-alist '("\\.php\\'"      . web-mode))
 (add-to-list 'auto-mode-alist '("\\.tsx\\'"      . web-mode))
