1cc6f0bb42fcd1be95465760d7a7844d7ab23045
ncaq <ncaq@ncaq.net>
2017-12-21 13:30:50 +0900

    added: web-mode to json

--- profile/90_web.el
+++ profile/90_web.el
@@ -10,15 +10,14 @@
 
 (add-to-list 'auto-mode-alist '("\\.[agj]sp\\'"  . web-mode))
 (add-to-list 'auto-mode-alist '("\\.as[cp]x\\'"  . web-mode))
+(add-to-list 'auto-mode-alist '("\\.djhtml\\'"   . web-mode))
 (add-to-list 'auto-mode-alist '("\\.erb\\'"      . web-mode))
-(add-to-list 'auto-mode-alist '("\\.js\\'"       . web-mode))
-(add-to-list 'auto-mode-alist '("\\.jsx\\'"      . web-mode))
+(add-to-list 'auto-mode-alist '("\\.html?\\'"    . web-mode))
+(add-to-list 'auto-mode-alist '("\\.json\\'"     . web-mode))
 (add-to-list 'auto-mode-alist '("\\.mustache\\'" . web-mode))
 (add-to-list 'auto-mode-alist '("\\.php\\'"      . web-mode))
-(add-to-list 'auto-mode-alist '("\\.tsx\\'"      . web-mode))
-(add-to-list 'auto-mode-alist '("html?\\'"       . web-mode))
+(add-to-list 'auto-mode-alist '("\\.phtml\\'"    . web-mode))
+(add-to-list 'auto-mode-alist '("\\.tpl\\'"      . web-mode))
 
 (with-eval-after-load 'web-mode (sp-local-pair '(web-mode) "<" ">" :actions :rem))
 
-(defun json-ncaq-setup () (setq-local js-indent-level 2))
-(add-hook 'json-mode-hook 'json-ncaq-setup)
