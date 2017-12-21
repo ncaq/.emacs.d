f1953382083ebfc51f50ac7170f3744039bbf5d5
ncaq <ncaq@ncaq.net>
2017-12-21 15:34:37 +0900

    deleted: js2-mode

--- profile/90_web.el
+++ profile/90_web.el
@@ -1,22 +1,10 @@
 ;; -*- lexical-binding: t -*-
 
 (custom-set-variables
- '(js2-highlight-external-variables nil)
- '(js2-ignored-warnings t)
- '(js2-include-browser-externs nil)
- '(js2-include-jslint-globals nil)
- '(js2-mode-show-parse-errors nil)
- '(js2-mode-show-strict-warnings nil)
  '(web-mode-enable-current-column-highlight t)
  '(web-mode-enable-current-element-highlight t)
  '(web-mode-markup-indent-offset 2)
  )
-
-(add-to-list 'auto-mode-alist '("\\.js\\'" . js2-mode))
-(add-to-list 'auto-mode-alist '("\\.jsx\\'" . js2-jsx-mode))
-
-(add-hook 'js2-mode-hook 'prettier-js-mode)
-(add-hook 'js2-jsx-mode-hook 'prettier-js-mode)
 
 (put 'web-mode-markup-indent-offset 'safe-local-variable 'integerp)
 
@@ -25,10 +13,19 @@
 (add-to-list 'auto-mode-alist '("\\.djhtml\\'"   . web-mode))
 (add-to-list 'auto-mode-alist '("\\.erb\\'"      . web-mode))
 (add-to-list 'auto-mode-alist '("\\.html?\\'"    . web-mode))
+(add-to-list 'auto-mode-alist '("\\.js\\'"       . web-mode))
 (add-to-list 'auto-mode-alist '("\\.json\\'"     . web-mode))
+(add-to-list 'auto-mode-alist '("\\.jsx\\'"      . web-mode))
 (add-to-list 'auto-mode-alist '("\\.mustache\\'" . web-mode))
 (add-to-list 'auto-mode-alist '("\\.php\\'"      . web-mode))
 (add-to-list 'auto-mode-alist '("\\.phtml\\'"    . web-mode))
 (add-to-list 'auto-mode-alist '("\\.tpl\\'"      . web-mode))
+
+(flycheck-add-mode 'javascript-eslint 'web-mode)
+(add-hook 'web-mode-hook '(lambda ()
+                            (if (or
+                                 (string= web-mode-content-type "javascript")
+                                 (string= web-mode-content-type "jsx"))
+                                (prettier-js-mode))))
 
 (with-eval-after-load 'web-mode (sp-local-pair '(web-mode) "<" ">" :actions :rem))
