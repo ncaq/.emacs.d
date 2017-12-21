677adae334aa4b69f410dd159367b69c93a4231e
ncaq <ncaq@ncaq.net>
2017-12-21 13:36:56 +0900

    added: js2-mode setting

--- profile/90_web.el
+++ profile/90_web.el
@@ -1,10 +1,22 @@
 ;; -*- lexical-binding: t -*-
 
 (custom-set-variables
+ '(js2-highlight-external-variables nil)
+ '(js2-ignored-warnings t)
+ '(js2-include-browser-externs nil)
+ '(js2-include-jslint-globals nil)
+ '(js2-mode-show-parse-errors nil)
+ '(js2-mode-show-strict-warnings nil)
  '(web-mode-enable-current-column-highlight t)
  '(web-mode-enable-current-element-highlight t)
  '(web-mode-markup-indent-offset 2)
  )
+
+(add-to-list 'auto-mode-alist '("\\.js\\'" . js2-mode))
+(add-to-list 'auto-mode-alist '("\\.jsx\\'" . js2-jsx-mode))
+
+(add-hook 'js2-mode-hook 'prettier-js-mode)
+(add-hook 'js2-jsx-mode-hook 'prettier-js-mode)
 
 (put 'web-mode-markup-indent-offset 'safe-local-variable 'integerp)
 
@@ -20,4 +32,3 @@
 (add-to-list 'auto-mode-alist '("\\.tpl\\'"      . web-mode))
 
 (with-eval-after-load 'web-mode (sp-local-pair '(web-mode) "<" ">" :actions :rem))
-
