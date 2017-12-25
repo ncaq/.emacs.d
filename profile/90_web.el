3499cd096895a67e7353806e542696ff6ee7e670
ncaq <ncaq@ncaq.net>
2017-12-25 16:53:25 +0900

    deleted: jsonにweb-modeを使うのはやめる

--- profile/90_web.el
+++ profile/90_web.el
@@ -14,7 +14,6 @@
 (add-to-list 'auto-mode-alist '("\\.erb\\'"      . web-mode))
 (add-to-list 'auto-mode-alist '("\\.html?\\'"    . web-mode))
 (add-to-list 'auto-mode-alist '("\\.js\\'"       . web-mode))
-(add-to-list 'auto-mode-alist '("\\.json\\'"     . web-mode))
 (add-to-list 'auto-mode-alist '("\\.jsx\\'"      . web-mode))
 (add-to-list 'auto-mode-alist '("\\.mustache\\'" . web-mode))
 (add-to-list 'auto-mode-alist '("\\.php\\'"      . web-mode))
