5e4dd55ee26a5ab1a29b14a118443fdb554e1861
ncaq <ncaq@ncaq.net>
2018-02-22 15:25:30 +0900

    modified: number-to-stringを使う
    int-to-stringはnumber-to-stringの別名.

--- profile/50_toolkit.el
+++ profile/50_toolkit.el
@@ -5,14 +5,23 @@
 
 (auto-image-file-mode 1);画像表示
 
-(setq frame-title-format '(:eval (list (or (buffer-file-name) (buffer-name)) " " mode-line-modes))) ;((ファイル名 or バッファ名) モード一覧)
+;; ((ファイル名 or バッファ名) モード一覧)
+(setq frame-title-format '(:eval (list (or (buffer-file-name) (buffer-name)) " " mode-line-modes)))
 
 ;; mode-line line and column and sum char numbar
-(setq mode-line-position '(:eval
-                           (list "l%l/" (int-to-string (count-lines (point-min) (point-max))) " "
-                                 "c" (int-to-string (- (point) (line-beginning-position))) "/" (int-to-string (- (line-end-position) (line-beginning-position))) " "
-                                 "s" (int-to-string (point)) "/%i"
-                                 )))
+(setq mode-line-position
+      '(:eval
+        (list
+         "l%l/"
+         (number-to-string (count-lines (point-min) (point-max)))
+         " c"
+         (number-to-string (- (point) (line-beginning-position)))
+         "/"
+         (number-to-string (- (line-end-position) (line-beginning-position)))
+         " s"
+         (number-to-string (point))
+         "/%i"
+         )))
 
 (require 'uniquify)
 (custom-set-variables '(uniquify-buffer-name-style 'forward))
