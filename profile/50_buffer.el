7a92eca844b90fc83ee6d834d4f690be5f0a6ac6
ncaq <ncaq@ncaq.net>
2018-02-22 14:58:38 +0900

    added: 'web-mode-hook 'rainbow-delimiters-mode-enable
    web-mode-hookはprog-mode以下じゃないっぽい?

--- profile/50_buffer.el
+++ profile/50_buffer.el
@@ -13,6 +13,7 @@
 
 (require 'rainbow-delimiters)
 (add-hook 'prog-mode-hook 'rainbow-delimiters-mode-enable)
+(add-hook 'web-mode-hook 'rainbow-delimiters-mode-enable)
 (set-face-foreground 'rainbow-delimiters-depth-1-face "#586e75") ;文字列の色と被るため,変更
 
 (autoload 'rainbow-turn-on "rainbow-mode")
