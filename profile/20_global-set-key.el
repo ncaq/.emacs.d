08224463050683f9974e6e1dfdd1c10c40e3d1c2
ncaq <ncaq@ncaq.net>
2018-03-09 16:50:15 +0900

    added: copy-to-register-@, insert-register-@

--- profile/20_global-set-key.el
+++ profile/20_global-set-key.el
@@ -51,7 +51,8 @@
 (global-set-key (kbd "C-M-o") 'ibuffer)
 (global-set-key (kbd "C-M-p") 'split-window-dwim-and-other)
 (global-set-key (kbd "C-M-q") 'kill-this-buffer)
-(global-set-key (kbd "C-M-w") 'kill-region-or-sexp-at-point)
+(global-set-key (kbd "C-M-w") 'copy-to-register-@)
+(global-set-key (kbd "C-M-y") 'insert-register-@)
 
 (global-set-key (kbd "C-M-S-q") 'kill-all-buffers)
 
