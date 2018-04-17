d4a77e7f295957dbbd88071fc15c8aa379cad7ac
ncaq <ncaq@ncaq.net>
2018-04-17 15:03:06 +0900

    deleted: haskell-indentation-where-{pre,post}-offset
    デフォルトで2だから設定書く必要無かった.

--- profile/90_haskell.el
+++ profile/90_haskell.el
@@ -4,8 +4,6 @@
  '(haskell-indentation-layout-offset 4)
  '(haskell-indentation-left-offset 4)
  '(haskell-indentation-starter-offset 4)
- '(haskell-indentation-where-post-offset 2)
- '(haskell-indentation-where-pre-offset 2)
  '(haskell-stylish-on-save t)
  '(intero-global-mode 1)
  )
