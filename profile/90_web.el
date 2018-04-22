e890312c8b3ab2708dc15435aa20b573ac432bb4
ncaq <ncaq@ncaq.net>
2018-04-22 18:33:30 +0900

    changed: json to json-mode, web-mode use tslint

--- profile/90_web.el
+++ profile/90_web.el
@@ -17,7 +17,6 @@
 (add-to-list 'auto-mode-alist '("\\.erb\\'"      . web-mode))
 (add-to-list 'auto-mode-alist '("\\.html?\\'"    . web-mode))
 (add-to-list 'auto-mode-alist '("\\.js\\'"       . web-mode))
-(add-to-list 'auto-mode-alist '("\\.json\\'"     . web-mode))
 (add-to-list 'auto-mode-alist '("\\.jsx\\'"      . web-mode))
 (add-to-list 'auto-mode-alist '("\\.mustache\\'" . web-mode))
 (add-to-list 'auto-mode-alist '("\\.php\\'"      . web-mode))
@@ -29,11 +28,13 @@
 
 (flycheck-add-mode 'html-tidy 'web-mode)
 (flycheck-add-mode 'javascript-eslint 'web-mode)
+(flycheck-add-mode 'typescript-tslint 'web-mode)
 
 (defun web-mode-setting ()
   (cond
    ((string= web-mode-content-type "html")
     (when (executable-find "tidy") (flycheck-select-checker 'html-tidy)))
+   ((string-match-p "\\.tsx?$" (buffer-file-name)) (flycheck-select-checker 'typescript-tslint))
    ((or (some (lambda (type) (string= web-mode-content-type type)) '("javascript" "jsx")))
     (when (executable-find "eslint") (flycheck-select-checker 'javascript-eslint))))
   (when
