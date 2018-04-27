f0509e045fdbe7c7504458822345d2fc0d0d5ab5
ncaq <ncaq@ncaq.net>
2018-04-27 13:02:26 +0900

    changed: smartparens: keybind

--- profile/60_smartparens.el
+++ profile/60_smartparens.el
@@ -12,8 +12,7 @@
   (newline)
   (indent-according-to-mode)
   (forward-line -1)
-  (indent-according-to-mode)
-  )
+  (indent-according-to-mode))
 
 (sp-pair "{" nil :post-handlers '((my-create-newline-and-enter-sexp "RET")))
 
@@ -22,12 +21,16 @@
 (define-key smartparens-mode-map (kbd "M-(") 'sp-backward-barf-sexp)
 (define-key smartparens-mode-map (kbd "M-)") 'sp-forward-barf-sexp)
 
-(define-key smartparens-mode-map (kbd "M-u") 'sp-splice-sexp)
-(define-key smartparens-mode-map (kbd "C-M-u") 'sp-up-sexp)
+(define-key smartparens-mode-map (kbd "M-k") 'sp-splice-sexp)
 
 (define-key smartparens-mode-map (kbd "C-M-k") 'sp-raise-sexp)
+(define-key smartparens-mode-map (kbd "C-M-u") 'sp-split-sexp)
 
-(define-key smartparens-mode-map [remap forward-sexp]       'sp-forward-sexp)
-(define-key smartparens-mode-map [remap backward-sexp]      'sp-backward-sexp)
-(define-key smartparens-mode-map [remap kill-sexp]          'sp-kill-sexp)
 (define-key smartparens-mode-map [remap backward-kill-sexp] 'sp-backward-kill-sexp)
+(define-key smartparens-mode-map [remap backward-list]      'sp-backward-symbol)
+(define-key smartparens-mode-map [remap backward-sexp]      'sp-backward-sexp)
+(define-key smartparens-mode-map [remap beginning-of-defun] 'sp-backward-down-sexp)
+(define-key smartparens-mode-map [remap end-of-defun]       'sp-down-sexp)
+(define-key smartparens-mode-map [remap forward-list]       'sp-forward-symbol)
+(define-key smartparens-mode-map [remap forward-sexp]       'sp-forward-sexp)
+(define-key smartparens-mode-map [remap kill-sexp]          'sp-kill-sexp)
