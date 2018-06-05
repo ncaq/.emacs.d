ebcae828ca0ece9baa7557708c432d73aa086c77
ncaq <ncaq@ncaq.net>
2018-06-05 19:43:14 +0900

    deleted: haskell indent: デフォルト2にしてしまう

--- profile/90_haskell.el
+++ profile/90_haskell.el
@@ -1,9 +1,6 @@
 ;; -*- lexical-binding: t -*-
 
 (custom-set-variables
- '(haskell-indentation-layout-offset 4)
- '(haskell-indentation-left-offset 4)
- '(haskell-indentation-starter-offset 4)
  '(haskell-stylish-on-save t)
  '(intero-global-mode 1)
  )
