a410fd1b74649c3d90b0494be2ce4d1c9d676ce9
ncaq <ncaq@ncaq.net>
2018-07-01 22:55:22 +0900

    renamed: save-buffer-and-dfmt -> save-buffer-and-dfmt

--- profile/90_d.el
+++ profile/90_d.el
@@ -5,15 +5,15 @@
    '(c-default-style (cons '(d-mode . "java") c-default-style))
    '(dfmt-flags '("--max_line_length=80"))
    )
-  (defun dfmt-save-buffer-force ()
+  (defun save-buffer-and-dfmt ()
     "セーブした後dfmt-bufferする.
 dfmt-bufferを先にしたりbefore-save-hookを使ったりすると
 保存がキャンセルされてflycheckの恩恵を受けられない
 "
     (interactive)
-    (save-buffer)
-    (dfmt-buffer)
-    (when (buffer-modified-p) (save-buffer)))
+    (when (buffer-modified-p)
+      (save-buffer)
+      (when (and (dfmt-buffer) (buffer-modified-p)) (save-buffer))))
   (define-key d-mode-map [remap indent-whole-buffer] 'dfmt-region-or-buffer)
-  (define-key d-mode-map [remap save-buffer] 'dfmt-save-buffer-force)
+  (define-key d-mode-map [remap save-buffer] 'save-buffer-and-dfmt)
   )
