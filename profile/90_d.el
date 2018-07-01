aa1afeff73fedce3473621c5eae83c05f30c2c6c
ncaq <ncaq@ncaq.net>
2018-07-01 21:53:07 +0900

    added: dfmt: dfmt-save-buffer-force

--- profile/90_d.el
+++ profile/90_d.el
@@ -1,12 +1,16 @@
 ;; -*- lexical-binding: t -*-
 
 (with-eval-after-load 'd-mode
+  (custom-set-variables '(c-default-style (cons '(d-mode . "java") c-default-style)))
+  (defun dfmt-save-buffer-force ()
+    "セーブした後dfmt-bufferする.
+dfmt-bufferを先にしたりbefore-save-hookを使ったりすると
+保存がキャンセルされてflycheckの恩恵を受けられない
+"
+    (interactive)
+    (save-buffer)
+    (dfmt-buffer)
+    (when (buffer-modified-p) (save-buffer)))
   (define-key d-mode-map [remap indent-whole-buffer] 'dfmt-region-or-buffer)
-
-  (defun ncaq-d-setup ()
-    (custom-set-variables '(c-basic-offset 4))
-    (add-hook 'before-save-hook 'dfmt-buffer nil t)
-    )
-
-  (add-hook 'd-mode-hook 'ncaq-d-setup)
+  (define-key d-mode-map [remap save-buffer] 'dfmt-save-buffer-force)
   )
