b442676e8a193600ce5ccdb80331720cf44232e7
ncaq <ncaq@ncaq.net>
2018-07-19 18:07:44 +0900

    added: Rubyの文字コードマジックコメント自動挿入を無効化

--- profile/90_ruby.el
+++ profile/90_ruby.el
@@ -3,6 +3,7 @@
 (custom-set-variables
  '(inf-ruby-default-implementation "pry")
  '(inf-ruby-eval-binding "Pry.toplevel_binding")
+ '(ruby-insert-encoding-magic-comment nil)
  )
 
 (with-eval-after-load 'ruby-mode (ncaq-set-key ruby-mode-map))
