458d32a750008c2ebdc9042b64f392448a14fb0a
ncaq <ncaq@ncaq.net>
2018-07-20 16:02:54 +0900

    added: etc comment

--- profile/60_etc.el
+++ profile/60_etc.el
@@ -1,28 +1,50 @@
 ;; -*- lexical-binding: t -*-
 
 (custom-set-variables
+ ;; Manページを現在のウィンドウで表示
  '(Man-notify-method 'bully)
- '(blink-matching-paren nil)            ;括弧移動無効
- '(delete-by-moving-to-trash t)         ;ごみ箱を有効
- '(diff-switches "-u")                  ;diffをunifitedモードで
+ ;; 括弧移動無効
+ '(blink-matching-paren nil)
+ ;; ごみ箱を有効
+ '(delete-by-moving-to-trash t)
+ ;; diffをunifitedモードで
+ '(diff-switches "-u")
+ ;; 一部のコマンドを有効にする
  '(disabled-command-function nil)
+ ;; ediffでウィンドウを横分割
  '(ediff-split-window-function 'split-window-horizontally)
+ ;; ediffにframeを生成させない
  '(ediff-window-setup-function 'ediff-setup-windows-plain)
- '(global-auto-revert-mode 1)           ;自動再読込
+ ;; 自動再読込
+ '(global-auto-revert-mode 1)
+ ;; google翻訳のソースを英語に
  '(google-translate-default-source-language "en")
+ ;; google翻訳の対象を日本語に
  '(google-translate-default-target-language "ja")
+ ;; dotファイルで自動セミコロン挿入しない
  '(graphviz-dot-auto-indent-on-semi nil)
- '(imaxima-fnt-size "LARGE")                ;maximaの文字を大きく
- '(indent-tabs-mode nil)                    ;インデントをスペースで行う
- '(read-buffer-completion-ignore-case t)    ;大文字と小文字を区別しない バッファ名
- '(read-file-name-completion-ignore-case t) ;大文字と小文字を区別しない ファイル名
- '(require-final-newline t)                 ;POSIX改行
+ ;; maximaの文字を大きく
+ '(imaxima-fnt-size "LARGE")
+ ;; インデントをスペースで行う
+ '(indent-tabs-mode nil)
+ ;; 大文字と小文字を区別しない バッファ名
+ '(read-buffer-completion-ignore-case t)
+ ;; 大文字と小文字を区別しない ファイル名
+ '(read-file-name-completion-ignore-case t)
+ ;; ファイルの最後に改行
+ '(require-final-newline t)
+ ;; schemeの処理系はgauche
  '(scheme-program-name "gosh")
- '(scroll-step 1)                       ;最下段までScrollしてもカーソルを中心に戻さない
+ ;; 最下段までスクロールしてもカーソルを中心に戻さない
+ '(scroll-step 1)
+ ;; skypeのid
  '(skype--my-user-handle "ncaq__")
- '(vc-follow-symlinks t)                ;常にシンボリックリンクをたどる
- '(warning-minimum-level :error)        ;quiet waring
- '(x-select-enable-clipboard t)         ;クリップボードをX11と共有
+ ;; 常にシンボリックリンクをたどる
+ '(vc-follow-symlinks t)
+ ;; 警告をエラーレベルでないと表示しない
+ '(warning-minimum-level :error)
+ ;; クリップボードをX11と共有
+ '(x-select-enable-clipboard t)
  )
 
 ;;スクリプトに実行権限付加
