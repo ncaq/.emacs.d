194d85632f056e4eba129865a75a299a168d0d72
ncaq <ncaq@ncaq.net>
2018-09-10 16:25:25 +0900

    deleted: flycheck-check-syntax-automatically: eslint-fixと相性が悪い

--- profile/60_flycheck.el
+++ profile/60_flycheck.el
@@ -4,7 +4,6 @@
 
 (custom-set-variables
  '(global-flycheck-mode t)
- '(flycheck-check-syntax-automatically '(mode-enabled save)) ;セーブした時だけにリロード
  '(flycheck-display-errors-function nil)                     ;Echoエリアにエラーを表示しない
  '(flycheck-highlighting-mode nil)                           ;下線があると_が見えなくなる
  '(flycheck-indication-mode 'left-fringe)
