102631615f7b06886669e050103c66bb78d98062
ncaq <ncaq@ncaq.net>
2018-09-13 17:13:00 +0900

    added: 再帰関数の制限を緩くする

--- init.el
+++ init.el
@@ -1,8 +1,12 @@
 ;; -*- lexical-binding: t -*-
 
 (custom-set-variables
+ ;; gcの起動条件を緩くしてメモリを犠牲にパフォーマンスを向上
  '(gc-cons-percentage (* gc-cons-percentage 5))
  '(gc-cons-threshold (* gc-cons-threshold 5))
+ ;; 再帰回数の制限緩くしてフリーズの危険性を犠牲にエラー落ちを減らす
+ '(max-lisp-eval-depth (* max-lisp-eval-depth 20))
+ '(max-specpdl-size (* max-specpdl-size 20))
  )
 
 (require 'package)
