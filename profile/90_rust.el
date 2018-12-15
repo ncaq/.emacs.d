b9d33f2b351579e255e6c9ae2ee4114d6480cbcc
ncaq <ncaq@ncaq.net>
2018-12-15 14:18:10 +0900

    added: lsp-rust-enable work around

--- profile/90_rust.el
+++ profile/90_rust.el
@@ -7,5 +7,10 @@
 (add-hook 'rustic-mode-hook 'racer-mode)
 (add-hook 'racer-mode-hook 'eldoc-mode)
 
+;; lsp-rust-enableが消滅したのでバッドノウハウとしてダミーの関数を定義する
+(unless (fboundp 'lsp-rust-enable)
+  (defun lsp-rust-enable ()
+    ()))
+
 (with-eval-after-load 'racer
   (define-key racer-mode-map (kbd "C-c C-d") 'racer-describe))
