c955912444b0e48628b1349315fbd9b6c43b2c80
ncaq <ncaq@ncaq.net>
2018-12-15 16:18:10 +0900

    added: cl-delete-if rust-mode
    rustic-modeで開きたい.

--- profile/90_rust.el
+++ profile/90_rust.el
@@ -2,6 +2,9 @@
 
 (require 'lsp-mode)
 
+;; rust-modeで開かれる時があるのでrustic-modeを末尾に追加し直す
+(cl-delete-if (lambda (element) (equal (cdr element) 'rust-mode)) auto-mode-alist)
+(cl-delete-if (lambda (element) (equal (cdr element) 'rustic-mode)) auto-mode-alist)
 (add-to-list 'auto-mode-alist '("\\.rs$" . rustic-mode))
 
 (add-hook 'rustic-mode-hook 'racer-mode)
