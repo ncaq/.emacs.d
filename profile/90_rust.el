44d32c85432b8605f196184ab346bb4d21dd09a3
ncaq <ncaq@ncaq.net>
2018-12-17 20:14:19 +0900

    deleted: rust relation package

--- profile/90_rust.el
+++ profile/90_rust.el
@@ -1,19 +1,1 @@
 ;; -*- lexical-binding: t -*-
-
-(require 'lsp-mode)
-
-;; rust-modeで開かれる時があるのでrustic-modeを末尾に追加し直す
-(cl-delete-if (lambda (element) (equal (cdr element) 'rust-mode)) auto-mode-alist)
-(cl-delete-if (lambda (element) (equal (cdr element) 'rustic-mode)) auto-mode-alist)
-(add-to-list 'auto-mode-alist '("\\.rs$" . rustic-mode))
-
-(add-hook 'rustic-mode-hook 'racer-mode)
-(add-hook 'racer-mode-hook 'eldoc-mode)
-
-;; lsp-rust-enableが消滅したのでバッドノウハウとしてダミーの関数を定義する
-(unless (fboundp 'lsp-rust-enable)
-  (defun lsp-rust-enable ()
-    ()))
-
-(with-eval-after-load 'racer
-  (define-key racer-mode-map (kbd "C-c C-d") 'rustic-racer-describe))
