f165b47e38be9b4ad7999946ef009da3981b6169
ncaq <ncaq@ncaq.net>
2019-01-18 15:08:29 +0900

    added: eglot: rename, format

--- profile/60_eglot.el
+++ profile/60_eglot.el
@@ -2,5 +2,7 @@
 
 (with-eval-after-load 'eglot
   (define-key eglot-mode-map (kbd "C-c C-d") 'eglot-help-at-point)
+  (define-key eglot-mode-map (kbd "C-c C-n") 'eglot-rename)
+  (define-key eglot-mode-map (kbd "C-c C-i") 'eglot-format)
   (define-key eglot-mode-map (kbd "C-c C-r") 'eglot-code-actions)
   )
