b91d22f79ded0cd85223e55d2fe2544665b9b91e
ncaq <ncaq@ncaq.net>
2019-05-15 15:56:19 +0900

    added: ロックファイルを作成しない

--- profile/50_history.el
+++ profile/50_history.el
@@ -4,16 +4,20 @@
 (require 'recentf-remove-sudo-tramp-prefix)
 
 (custom-set-variables
+ ;; バックアップ先をカレントディレクトリから変更
  '(backup-directory-alist `(("" . ,(concat user-emacs-directory "file-backup/"))))
- '(delete-old-versions t)            ;askだと削除時に一々聞いてくる
- '(kept-new-versions 50)             ;backupに新しいものをいくつ残すか
- '(kept-old-versions 0)              ;backupに古いものをいくつ残すか
- '(make-backup-files t)              ;バックアップファイルを作成する。
- '(vc-make-backup-files t)           ;VCS以下のファイルもバックアップを作成する
- '(version-control t)                ;複数バックアップ
 
+ ;; 自動保存(クラッシュ時の対応)先をカレントディレクトリから変更
  '(auto-save-file-name-transforms `((".*" ,temporary-file-directory t)))
  '(tramp-auto-save-directory temporary-file-directory)
+
+ '(create-lockfiles nil)                ; ロックファイルとしてシンボリックリンクを作らない.parcelが大変なことになるので.
+ '(delete-old-versions t)               ; askだと削除時に一々聞いてくる
+ '(kept-new-versions 50)                ; backupに新しいものをいくつ残すか
+ '(kept-old-versions 0)                 ; backupに古いものをいくつ残すか
+ '(make-backup-files t)                 ; バックアップファイルを作成する。
+ '(vc-make-backup-files t)              ; VCS以下のファイルもバックアップを作成する
+ '(version-control t)                   ; 複数バックアップ
 
  '(message-log-max 100000)
 
