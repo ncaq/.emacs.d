70301a97e97f8bbb322a2b3675dbbbb8759dbb07
ncaq <ncaq@ncaq.net>
2019-05-21 14:59:44 +0900

    added: eglot-reconnect

--- profile/60_eglot.el
+++ profile/60_eglot.el
@@ -2,8 +2,9 @@
 
 (with-eval-after-load 'eglot
   (define-key eglot-mode-map (kbd "C-c C-d") 'eglot-help-at-point)
-  (define-key eglot-mode-map (kbd "C-c C-n") 'eglot-rename)
+  (define-key eglot-mode-map (kbd "C-c C-e") 'eglot-reconnect)
   (define-key eglot-mode-map (kbd "C-c C-i") 'eglot-format)
+  (define-key eglot-mode-map (kbd "C-c C-n") 'eglot-rename)
   (define-key eglot-mode-map (kbd "C-c C-r") 'eglot-code-actions)
   )
 
