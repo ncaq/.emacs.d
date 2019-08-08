d9938e8c85fd051a69d02fe5b37a74c5689ce504
ncaq <ncaq@ncaq.net>
2019-08-08 21:53:36 +0900

    added: ejs to web-mode

--- profile/90_web.el
+++ profile/90_web.el
@@ -16,6 +16,7 @@
 (add-to-list 'auto-mode-alist '("\\.[agj]sp\\'"  . web-mode))
 (add-to-list 'auto-mode-alist '("\\.as[cp]x\\'"  . web-mode))
 (add-to-list 'auto-mode-alist '("\\.djhtml\\'"   . web-mode))
+(add-to-list 'auto-mode-alist '("\\.ejs\\'"      . web-mode))
 (add-to-list 'auto-mode-alist '("\\.erb\\'"      . web-mode))
 (add-to-list 'auto-mode-alist '("\\.html?\\'"    . web-mode))
 (add-to-list 'auto-mode-alist '("\\.js\\'"       . web-mode))
