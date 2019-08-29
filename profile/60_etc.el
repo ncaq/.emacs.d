ea13ac724d5b9957a6862a5ae9316dc226f60f55
ncaq <ncaq@ncaq.net>
2019-08-29 22:31:16 +0900

    deleted: executable-make-buffer-file-executable-if-script-p

--- profile/60_etc.el
+++ profile/60_etc.el
@@ -49,7 +49,5 @@
  '(x-select-enable-clipboard t)
  )
 
-;;スクリプトに実行権限付加
-(add-hook 'after-save-hook 'executable-make-buffer-file-executable-if-script-p)
 ;;"yes or no"を"y or n"に
 (fset 'yes-or-no-p 'y-or-n-p)
