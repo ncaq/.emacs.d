bb4dc423008b9354b44d4e4bfbecabea20302f75
ncaq <ncaq@ncaq.net>
2019-09-09 16:06:08 +0900

    added: lsp for dockerfile

--- profile/60_docker.el
+++ profile/60_docker.el
@@ -1,3 +1,5 @@
 ;; -*- lexical-binding: t -*-
 
 (custom-set-variables '(docker-container-shell-file-name "/bin/bash"))
+
+(add-hook 'dockerfile-mode-hook 'eglot-ensure)
--- profile/60_eglot.el
+++ profile/60_eglot.el
@@ -2,6 +2,7 @@
 
 (with-eval-after-load 'eglot
   (add-to-list 'eglot-server-programs '((css-mode sass-mode scss-mode less-css-mode) . ("css-languageserver" "--stdio")))
+  (add-to-list 'eglot-server-programs '(dockerfile-mode . ("docker-langserver" "--stdio")))
   (add-to-list 'eglot-server-programs (cons 'web-mode (cdr (assoc '(js-mode typescript-mode) eglot-server-programs))))
 
   (define-key eglot-mode-map (kbd "C-c C-d") 'eglot-help-at-point)
