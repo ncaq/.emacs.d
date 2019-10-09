1765ab0aae8f0894673397d50157e277657c5a2d
ncaq <ncaq@ncaq.net>
2019-10-09 16:45:30 +0900

    added: markdown: markdown-code-lang-modes

--- profile/90_markdown.el
+++ profile/90_markdown.el
@@ -5,6 +5,25 @@
 (add-to-list 'auto-mode-alist '("\\.markdown\\'" . markdown-mode))
 
 (custom-set-variables
+ '(markdown-code-lang-modes
+   (append '(
+             ("diff" . diff-mode)
+             ("hs" . haskell-mode)
+             ("html" . web-mode)
+             ("ini" . conf-mode)
+             ("js" . web-mode)
+             ("jsx" . web-mode)
+             ("md" . markdown-mode)
+             ("pl6" . perl6-mode)
+             ("py" . python-mode)
+             ("rb" . ruby-mode)
+             ("rs" . rustic-mode)
+             ("sqlite3" . sql-mode)
+             ("ts" . typescript-mode)
+             ("tsx" . web-mode)
+             ("zsh" . sh-mode)
+             )
+           markdown-code-lang-modes))
  '(markdown-command "pandoc")
  '(markdown-fontify-code-blocks-natively t)
  '(markdown-hide-urls nil)
