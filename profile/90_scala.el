bd9860714411d37d22a1b62f4ca90c3607424402
ncaq <ncaq@ncaq.net>
2019-11-07 19:09:51 +0900

    deleted: ensime-mode
    ensimeの作者がmetalsのゴリ押しにブチギレてプロジェクトをやめてしまったため.
    [remove ensime and return scala-mode/sbt-mode to original author by fommil · Pull Request #6443 · melpa/melpa](https://github.com/melpa/melpa/pull/6443)

--- profile/90_scala.el
+++
@@ -1,9 +0,0 @@
-;; -*- lexical-binding: t -*-
-
-(with-eval-after-load 'ensime-startup
-  (custom-set-variables '(ensime-startup-notification nil)))
-
-(with-eval-after-load 'ensime-mode
-  (define-key ensime-mode-map (kbd "M-n") nil)
-  (define-key ensime-mode-map (kbd "M-p") nil)
-  )
