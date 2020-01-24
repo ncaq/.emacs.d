1a2ff03bd8eb4e81d823fcb59609b7c3163ac61a
ncaq <ncaq@ncaq.net>
2020-01-24 17:47:34 +0900

    added: scala-mode-setting

--- profile/90_scala.el
+++ profile/90_scala.el
@@ -1,4 +1,8 @@
 ;; -*- lexical-binding: t -*-
 
+(defun scala-mode-setting ()
+  (add-hook 'after-save-hook 'lsp-format-buffer nil t))
+
 (with-eval-after-load 'scala-mode
-  (add-hook 'scala-mode-hook 'lsp))
+  (add-hook 'scala-mode-hook 'lsp)
+  (add-hook 'scala-mode-hook 'scala-mode-setting))
