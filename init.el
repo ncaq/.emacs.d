fd029f22acce483d237a79ec4474969e035a4d9d
ncaq <ncaq@ncaq.net>
2020-04-27 17:31:40 +0900

    changed: use ghcide

--- init.el
+++ init.el
@@ -974,7 +974,8 @@ dfmt-bufferを先にしたりbefore-save-hookを使ったりすると
     :require t
     :custom
     (flymake-proc-allowed-file-name-masks . (delete '("\\.l?hs\\'" haskell-flymake-init) flymake-allowed-file-name-masks))
-    (lsp-haskell-process-path-hie . "haskell-language-server-wrapper"))
+    (lsp-haskell-process-args-hie . '())
+    (lsp-haskell-process-path-hie . "ghcide"))
   (leaf haskell-customize
     :custom (haskell-stylish-on-save . t)
     :config
