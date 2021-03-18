f46496549d49e0e882508610b1ad0a96dc312787
ncaq <ncaq@ncaq.net>
2021-03-18 19:45:36 +0900

    changed: mozc-preedit-selected-face: より背景色に近い色でハイライトします
    緑色がTN液晶で見たからか読みにくかった。

--- init.el
+++ init.el
@@ -839,7 +839,7 @@ python, ruby, rustはスネークケースを含むのでruby(pythonはrubyのal
   :custom
   (default-input-method . "japanese-mozc-im")
   (mozc-candidate-style . 'echo-area)
-  :custom-face (mozc-preedit-selected-face . '((t (:background "#268bd2")))))
+  :custom-face (mozc-preedit-selected-face . '((t (:background "#586e75")))))
 
 (leaf tr-ime
   :doc "C-mでの確定にはEmacs側で対応していないのでKeyHacなどでの対処が必要"
