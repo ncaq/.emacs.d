018e13a5eb6ea8a74a3955d61d5e58ccd6101313
ncaq <ncaq@ncaq.net>
2021-11-11 19:15:42 +0900

    added: lsp-haskell-importlens-on . nil

--- init.el
+++ init.el
@@ -1161,6 +1161,8 @@ python, ruby, rustはスネークケースを含むのでruby(pythonはrubyのal
     (lsp-haskell-completion-snippets-on . nil)
     ;; import補完はcompanyから誤爆する可能性が高すぎるので無効化します。
     (lsp-haskell-refineimports-on . nil)
+    ;; importされたものが出てくる機能自体の思想は分かり易くて良いのですが、スクロール周りがすごい面倒になるので無効化。
+    (lsp-haskell-importlens-on . nil)
     :defun
     lsp-code-actions-at-point
     lsp:code-action-title
