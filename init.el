9d7aa001067ffd762a66c5d8f65d9d2548518697
ncaq <ncaq@ncaq.net>
2022-07-04 12:58:11 +0900

    feat: delete: lsp-haskell-plugin-import-lens-code-lens-on
    これでも一部有効になってしまうようなので、
    スクロールも少し改善した気がするのでデフォルトに倒す。

--- init.el
+++ init.el
@@ -1177,8 +1177,6 @@ python, ruby, rustはスネークケースを含むのでruby(pythonはrubyのal
     (lsp-haskell-completion-snippets-on . nil)
     ;; 関数補完からの自動importはcompanyから誤爆する可能性が高すぎるので無効化します。
     (lsp-haskell-plugin-ghcide-completions-config-auto-extend-on . nil)
-    ;; importされたものが出てくる機能自体の思想は分かり易くて良いのですが、スクロール周りがすごい面倒になるので無効化。
-    (lsp-haskell-plugin-import-lens-code-lens-on . nil)
     :defun
     lsp-code-actions-at-point
     lsp:code-action-title
