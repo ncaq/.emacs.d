61adcff394e9d9336d7f8aa8854a3bd128b5aefa
ncaq <ncaq@ncaq.net>
2026-01-18 10:52:44 +0900

    build(dev-env): Nix Flakes対応の開発環境構築ファイルを追加
    - .envrcでdirenvとNix Flakesを利用した環境変数ロードを自動化
    - flake.nix/flake.lockでNix Flakesによる依存管理とフォーマッタ設定を導入
    - .gitignoreにNix Flakes関連ファイルと.envrcを追記し管理対象化

---
+++ flake.nix
@@ -0,0 +1,86 @@
+{
+  description = "My Emacs config";
+
+  inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-25.11";
+
+    flake-parts.url = "github:hercules-ci/flake-parts";
+
+    treefmt-nix = {
+      url = "github:numtide/treefmt-nix";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
+  };
+
+  outputs =
+    inputs@{
+      nixpkgs,
+      flake-parts,
+      treefmt-nix,
+      ...
+    }:
+    flake-parts.lib.mkFlake { inherit inputs; } {
+      imports = [
+        treefmt-nix.flakeModule
+      ];
+
+      systems = [
+        "x86_64-linux"
+      ];
+
+      perSystem =
+        {
+          pkgs,
+          inputs',
+          ...
+        }:
+        {
+          treefmt.config = {
+            projectRootFile = "flake.nix";
+            programs = {
+              actionlint.enable = true;
+              deadnix.enable = true;
+              nixfmt.enable = true;
+              prettier.enable = true;
+              shellcheck.enable = true;
+              shfmt.enable = true;
+              statix.enable = true;
+              zizmor.enable = true;
+            };
+            settings.formatter = {
+              editorconfig-checker = {
+                command = pkgs.lib.getExe (
+                  pkgs.writeShellApplication {
+                    name = "editorconfig-checker-wrapper";
+                    runtimeInputs = [ pkgs.editorconfig-checker ];
+                    text = ''
+                      editorconfig-checker -config .editorconfig-checker.json "$@"
+                    '';
+                  }
+                );
+                includes = [ "*" ];
+                excludes = [
+                  ".git/*"
+                  ".direnv/*"
+                  "result*"
+                ];
+              };
+            };
+          };
+          devShells.default = pkgs.mkShell { };
+        };
+    };
+
+  nixConfig = {
+    extra-substituters = [
+      "https://cache.nixos.org/"
+      "https://nix-community.cachix.org"
+      "https://cache.iog.io"
+    ];
+    extra-trusted-public-keys = [
+      "cache.nixos.org-1:6NCHdD59X431o0gWypbMrAURkbJ16ZPMQFGspcDShjY="
+      "nix-community.cachix.org-1:mB9FSh9qf2dCimDSUo8Zy7bkq5CX+/rkCWyvRCYg3Fs="
+      "hydra.iohk.io:f/Ea+s+dFdN+3Y/G+FDgSq+a5NEWhJGzdjvKNGv0/EQ="
+    ];
+  };
+}
