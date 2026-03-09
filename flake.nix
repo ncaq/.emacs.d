9dab06f3f3d0d4a5881a66fc13df224132561e93
ncaq <ncaq@ncaq.net>
2026-03-09 16:13:47 +0900

    build(flake): emacs-overlayを追加

--- flake.nix
+++ flake.nix
@@ -8,6 +8,13 @@
       url = "github:numtide/treefmt-nix";
       inputs.nixpkgs.follows = "nixpkgs";
     };
+    emacs-overlay = {
+      url = "github:nix-community/emacs-overlay";
+      inputs = {
+        nixpkgs.follows = "nixpkgs";
+        nixpkgs-stable.follows = "nixpkgs";
+      };
+    };
   };
 
   outputs =
