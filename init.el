edcc7957a4a567a22a1b098ab18a2ce841e3cd2f
ncaq <ncaq@ncaq.net>
2026-08-17 17:28:51 +0900

    fix: 設定変数のズレを修正

--- init.el
+++ init.el
@@ -1722,7 +1722,7 @@ PerlとPrologを自動識別するのは非常に有用です。"
  (leaf
   lsp-fsharp
   :custom
-  (lsp-fsharp-use-dotnet-local-tool . t))) ; nixで宣言的にインストールされてあることを前提にする。
+  (lsp-fsharp-use-dotnet-tool-for-fsac . t))) ; nixで宣言的にインストールされていることが前提
 
 ;;; Haskell
 
